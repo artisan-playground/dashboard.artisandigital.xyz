cd7417853b8b60ea9c00779094adda355b778826
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-07 16:02:06 +0700

    Side bar

--- web/src/components/SideNav.tsx
+++ web/src/components/SideNav.tsx
@@ -5,27 +5,43 @@ import {
   TeamOutlined,
   UserOutlined,
 } from '@ant-design/icons'
-import { Layout, Menu } from 'antd'
-import React from 'react'
 import { NavLink } from 'react-router-dom'
-import '../styles/main.css'
 
 const { SubMenu } = Menu
 const { Sider } = Layout
 
 function SideNav() {
   return (
-    <Sider className="bg-white min-h-screen shadow-lg">
-      <Menu mode="inline" defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} className="">
-        <Menu.Item key="1" icon={<ProfileOutlined />}>
-          <NavLink to="/">Dashboard</NavLink>
-        </Menu.Item>
-        <SubMenu key="sub2" icon={<ProjectOutlined />} title="Project">
-          <Menu.Item key="5">
-            <NavLink to="/project-list">All</NavLink>
+    <Layout>
+      <Sider width={240} className="site-layout-background min-h-screen shadow-lg">
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          className="h-full flex-col"
+        >
+          <Menu.Item key="1" icon={<ProfileOutlined />}>
+            <NavLink to="/project-list">Dashboard</NavLink>
           </Menu.Item>
-          <Menu.Item key="6">
-            <NavLink to="/project-list">New</NavLink>
+          <SubMenu key="sub2" icon={<ProjectOutlined />} title="Project">
+            <Menu.Item key="5">
+              <NavLink to="/project-list">All</NavLink>
+            </Menu.Item>
+            <Menu.Item key="6">
+              <NavLink to="/project-list">New</NavLink>
+            </Menu.Item>
+            <Menu.Item key="7">In Progress</Menu.Item>
+            <Menu.Item key="8">Closed</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="2" icon={<NotificationOutlined />}>
+            <NavLink to="/news">News</NavLink>
+          </Menu.Item>
+          <Menu.Item key="3" icon={<TeamOutlined />}>
+            <NavLink to="/member">Members</NavLink>
+          </Menu.Item>
+          <Menu.Item key="4" icon={<UserOutlined />}>
+            Profile
+            <NavLink to="/profile">Profile</NavLink>
           </Menu.Item>
           <Menu.Item key="7">In Progress</Menu.Item>
           <Menu.Item key="8">Closed</Menu.Item>
--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,16 +1,18 @@
 import { Layout } from 'antd'
 import React from 'react'
 import '../styles/main.css'
+import { Layout } from 'antd'
+
+const { Content } = Layout
 
 const { Content } = Layout
 
 function Dashboard() {
   return (
-    <Layout className="pt-8 pb-24 px-8 w-full ">
-      <Content className="bg-white p-8 w-full">
+    <Layout className="py-8 px-8 w-full">
+      <Content className="bg-white p-8 m-0">
         <div>
           <div>Dashboard</div>
-
           <button>
             <a href="/project-list">to project</a>
           </button>
