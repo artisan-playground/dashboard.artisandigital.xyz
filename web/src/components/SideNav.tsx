652d14036bd9bdaecf37deb89e3c3aee8f93824b
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-07 17:40:46 +0700

    Add dashboard component and layout

--- web/src/components/SideNav.tsx
+++ web/src/components/SideNav.tsx
@@ -12,29 +12,17 @@ const { Sider } = Layout
 
 function SideNav() {
   return (
-    <Layout>
-      <Sider width={240} className="site-layout-background min-h-screen shadow-lg">
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          className="h-full flex-col"
-        >
-          <Menu.Item key="1" icon={<ProfileOutlined />}>
-            <NavLink to="/project-list">Dashboard</NavLink>
+    <Sider className="bg-white min-h-screen shadow-lg">
+      <Menu mode="inline" defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} className="">
+        <Menu.Item key="1" icon={<ProfileOutlined />}>
+          <NavLink to="/project-list">Dashboard</NavLink>
+        </Menu.Item>
+        <SubMenu key="sub2" icon={<ProjectOutlined />} title="Project">
+          <Menu.Item key="5">
+            <NavLink to="/project-list">All</NavLink>
           </Menu.Item>
-          <SubMenu key="sub2" icon={<ProjectOutlined />} title="Project">
-            <Menu.Item key="5">
-              <NavLink to="/project-list">All</NavLink>
-            </Menu.Item>
-            <Menu.Item key="6">
-              <NavLink to="/project-list">New</NavLink>
-            </Menu.Item>
-            <Menu.Item key="7">In Progress</Menu.Item>
-            <Menu.Item key="8">Closed</Menu.Item>
-          </SubMenu>
-          <Menu.Item key="2" icon={<NotificationOutlined />}>
-            <NavLink to="/news">News</NavLink>
+          <Menu.Item key="6">
+            <NavLink to="/project-list">New</NavLink>
           </Menu.Item>
           <Menu.Item key="3" icon={<TeamOutlined />}>
             <NavLink to="/member">Members</NavLink>
