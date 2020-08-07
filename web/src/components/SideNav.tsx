09c975efec2688d39136f0af2092ceaf1820d9cf
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-07 14:34:05 +0700

    Side nav

--- web/src/components/SideNav.tsx
+++ web/src/components/SideNav.tsx
@@ -1,11 +1,47 @@
 import React from 'react'
 import '../styles/main.css'
+import { Layout, Menu } from 'antd'
+import {
+  UserOutlined,
+  ProfileOutlined,
+  NotificationOutlined,
+  ProjectOutlined,
+  TeamOutlined,
+} from '@ant-design/icons'
+
+const { SubMenu } = Menu
+const { Sider } = Layout
 
 function SideNav() {
   return (
-    <div className="absolute left-10 bg-gray-600 min-h-screen w-16 lg:w-64">
-      <div className="flex justify-center items-center h-64">Side Nav</div>
-    </div>
+    <Layout>
+      <Sider width={240} className="site-layout-background min-h-screen shadow-lg">
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          className="h-full"
+        >
+          <Menu.Item key="1" icon={<ProfileOutlined />}>
+            Dashboard
+          </Menu.Item>
+          <SubMenu key="sub2" icon={<ProjectOutlined />} title="Project">
+            <Menu.Item key="7">New</Menu.Item>
+            <Menu.Item key="5">In Progress</Menu.Item>
+            <Menu.Item key="6">Closed</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="2" icon={<NotificationOutlined />}>
+            News
+          </Menu.Item>
+          <Menu.Item key="3" icon={<TeamOutlined />}>
+            Members
+          </Menu.Item>
+          <Menu.Item key="4" icon={<UserOutlined />}>
+            Profile
+          </Menu.Item>
+        </Menu>
+      </Sider>
+    </Layout>
   )
 }
 
