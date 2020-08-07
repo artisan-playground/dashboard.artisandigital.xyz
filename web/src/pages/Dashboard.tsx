cf92e7b49d6af0e7dc04b7060083225ccd9a4d26
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-07 16:02:06 +0700

    Side bar

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import '../styles/main.css'
+import { Layout } from 'antd'
+
+const { Content } = Layout
 
 function Dashboard() {
   return (
-    <>
-      <div>
-        <div>Dashboard</div>
-        <button>
-          <a href="/project-list">to prolect</a>
-        </button>
-      </div>
-    </>
+    <Layout className="py-8 px-8 w-full">
+      <Content className="bg-white p-8 m-0">
+        <div>
+          <div>Dashboard</div>
+          <button>
+            <a href="/project-list">to project</a>
+          </button>
+        </div>
+      </Content>
+    </Layout>
   )
 }
 
