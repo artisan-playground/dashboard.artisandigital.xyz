ab63515be0c1961680951c8ab806669d12848d3d
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-07 18:03:38 +0700

    fix navbar

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -2,23 +2,25 @@ import { Layout } from 'antd'
 import React from 'react'
 import '../styles/main.css'
 
-import { Layout, Menu } from 'antd'
+import { Layout } from 'antd'
 
 const { Header } = Layout
 
 function NavBar() {
   return (
     <>
-      <Layout>
-        <Header className="header shadow-md">
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+      <Header className="header bg-white shadow-lg h-12">
+        <div className="flex-row justify-between">
+          <img className="w-40 h-10 mt-4" src={require('../assets/images/logo3.png')} alt="logo" />
+          <div>user</div>
+        </div>
+        {/* <div className="logo" /> */}
+        {/* <Menu theme="light" mode="horizontal">
             <Menu.Item key="1">nav 1</Menu.Item>
             <Menu.Item key="2">nav 2</Menu.Item>
             <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
-        </Header>
-      </Layout>
+          </Menu> */}
+      </Header>
     </>
   )
 }
