ff4800d59fdcdda35691fafbaa880db3eb80b96c
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-10 09:28:36 +0700

    Fixed side nav component

--- web/src/components/SideNav.tsx
+++ web/src/components/SideNav.tsx
@@ -13,34 +13,39 @@ import '../styles/main.css'
 const { SubMenu } = Menu
 const { Sider } = Layout
 
-function SideNav() {
+function SideNav({ children }: any) {
   return (
-    <Sider className="bg-white min-h-screen shadow-lg">
-      <Menu mode="inline" defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} className="">
-        <Menu.Item key="1" icon={<ProfileOutlined />}>
-          <NavLink to="/">Dashboard</NavLink>
-        </Menu.Item>
-        <SubMenu key="sub2" icon={<ProjectOutlined />} title="Project">
-          <Menu.Item key="5">
-            <NavLink to="/project-list">All</NavLink>
-          </Menu.Item>
-          <Menu.Item key="6">
-            <NavLink to="/project-list">New</NavLink>
-          </Menu.Item>
-          <Menu.Item key="7">In Progress</Menu.Item>
-          <Menu.Item key="8">Closed</Menu.Item>
-        </SubMenu>
-        <Menu.Item key="2" icon={<NotificationOutlined />}>
-          <NavLink to="/news">News</NavLink>
-        </Menu.Item>
-        <Menu.Item key="3" icon={<TeamOutlined />}>
-          <NavLink to="/member">Members</NavLink>
-        </Menu.Item>
-        <Menu.Item key="4" icon={<UserOutlined />}>
-          <NavLink to="/profile">Profile</NavLink>
-        </Menu.Item>
-      </Menu>
-    </Sider>
+    <Layout className="flex flex-row justify-start">
+      <div>
+        <Sider className="bg-white min-h-screen shadow-lg">
+          <Menu mode="inline" defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} className="">
+            <Menu.Item key="1" icon={<ProfileOutlined />}>
+              <NavLink to="/">Dashboard</NavLink>
+            </Menu.Item>
+            <SubMenu key="sub2" icon={<ProjectOutlined />} title="Project">
+              <Menu.Item key="5">
+                <NavLink to="/project-list">All</NavLink>
+              </Menu.Item>
+              <Menu.Item key="6">
+                <NavLink to="/project-list">New</NavLink>
+              </Menu.Item>
+              <Menu.Item key="7">In Progress</Menu.Item>
+              <Menu.Item key="8">Closed</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="2" icon={<NotificationOutlined />}>
+              <NavLink to="/news">News</NavLink>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<TeamOutlined />}>
+              <NavLink to="/member">Members</NavLink>
+            </Menu.Item>
+            <Menu.Item key="4" icon={<UserOutlined />}>
+              <NavLink to="/profile">Profile</NavLink>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+      </div>
+      <div className="min-h-screen w-full">{children}</div>
+    </Layout>
   )
 }
 
