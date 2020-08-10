5d26c0c6cd82494111b42e72b3f238a6012b2540
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-10 16:17:07 +0700

    Fix layout

--- web/src/components/LayoutDashboard.tsx
+++ web/src/components/LayoutDashboard.tsx
@@ -1,11 +1,18 @@
+import { Layout } from 'antd'
 import React from 'react'
 import { NavBar, SideNav } from './DashboardComponent'
+
+const { Content } = Layout
 
 function LayoutDashboard({ className, children }: any) {
   return (
     <>
       <NavBar />
-      <SideNav>{children}</SideNav>
+      <SideNav>
+        <Layout className="pt-8 pb-24 px-8 w-full ">
+          <Content className="bg-white p-8 w-full">{children}</Content>
+        </Layout>
+      </SideNav>
     </>
   )
 }
--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -10,42 +10,38 @@ const { Content } = Layout
 function Dashboard() {
   return (
     <LayoutDashboard>
-      <Layout className="pt-8 pb-24 px-8 w-full ">
-        <Content className="bg-white p-8 w-full">
-          <div>
-            <div className="font-bold text-2xl">Dashboard</div>
-            <div className="site-card-wrapper">
-              <Row gutter={16}>
-                <Col span={8}>
-                  <Card title="Card title" bordered={false} style={{ width: 300 }}>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                  </Card>
-                </Col>
-                <Col span={8}>
-                  <Card title="Card title" bordered={false} style={{ width: 300 }}>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                  </Card>
-                </Col>
-                <Col span={8}>
-                  <Card title="Card title" bordered={false} style={{ width: 300 }}>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                  </Card>
-                </Col>
-              </Row>
-            </div>
+      <div>
+        <div className="font-bold text-2xl">Dashboard</div>
+        <div className="site-card-wrapper">
+          <Row gutter={16}>
+            <Col span={8}>
+              <Card title="Card title" bordered={false} style={{ width: 300 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+            </Col>
+            <Col span={8}>
+              <Card title="Card title" bordered={false} style={{ width: 300 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+            </Col>
+            <Col span={8}>
+              <Card title="Card title" bordered={false} style={{ width: 300 }}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+            </Col>
+          </Row>
+        </div>
 
-            <button>
-              <a href="/project-list">to project</a>
-            </button>
-          </div>
-        </Content>
-      </Layout>
+        <button>
+          <a href="/project-list">to project</a>
+        </button>
+      </div>
     </LayoutDashboard>
   )
 }
