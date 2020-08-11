f995ea789111b71f8aed0de3ed4168575fcd276b
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-11 16:20:20 +0700

    Fix Dashboard card layout

--- web/src/components/ProjectCard.tsx
+++ web/src/components/ProjectCard.tsx
@@ -4,15 +4,18 @@ import React from 'react'
 const { Title, Text } = Typography
 function ProjectCard({ data }: any) {
   return (
-    <Card className="w-full rounded-lg">
-      <div className="absolute right-0 top-0 mt-4 mr-4">
+    <Card className="w-full rounded-lg shadow-md">
+      <div className="absolute right-0 top-0 mt-8 mr-4">
         {data.status === 'done' ? (
-          <Tag className="rounded-full py-1 px-2" icon={<CheckCircleOutlined />} color="success">
-            success
+          <Tag
+            className="rounded-full py-1 px-2 bg-successop"
+            icon={<CheckCircleOutlined className="animate-bounce" />}
+          >
+            <Text className="font-bold">success</Text>
           </Tag>
         ) : (
-          <Tag className="rounded-full py-1 px-2" icon={<SyncOutlined spin />} color="processing">
-            processing
+          <Tag className="rounded-full py-1 px-2 bg-progressop " icon={<SyncOutlined spin />}>
+            <Text className="font-bold"> In Progress</Text>
           </Tag>
         )}
       </div>
@@ -27,7 +30,7 @@ function ProjectCard({ data }: any) {
           </div>
         </Col>
         <Col span={8}>
-          <div className="absolute bottom-0 right-0 mb-4 br-4">
+          <div className="absolute bottom-0 right-0 mb-2 mr-2">
             {data.team.map((item: any) => {
               return <Avatar src={item.image} className="ml-2" alt={item.name} />
             })}
