1a4eb3e109d63ab49180a96f75c7845a55d2d9b8
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-13 17:58:54 +0700

    Sorted by type

--- web/src/pages/ProjectList.tsx
+++ web/src/pages/ProjectList.tsx
@@ -1,18 +1,51 @@
-import React from 'react'
+import { Col, Row } from 'antd'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
-import { LayoutDashboard } from '../components/DashboardComponent'
+import { LayoutDashboard, ProjectCard } from '../components/DashboardComponent'
+import { DATA } from '../DATA'
 
 function ProjectList() {
   const location = useLocation()
+  const [filteredData, setFilteredData] = useState<any[]>([])
 
   function getTypes() {
     const params = new URLSearchParams(location.search)
     return params.get('types') ? params.get('types') : ''
   }
 
+  useEffect(() => {
+    switch (getTypes()) {
+      case 'wip':
+        let wip: any[] = DATA.filter((item) => item.status === 'undone')
+        setFilteredData(wip)
+        break
+      case 'closed':
+        let closed: any[] = DATA.filter((item) => item.status === 'done')
+        setFilteredData(closed)
+        break
+      case '':
+        setFilteredData(DATA)
+        break
+      default:
+        setFilteredData(DATA)
+        break
+    }
+  }, [getTypes()]) // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <LayoutDashboard>
-      <div>Project list {getTypes()}</div>
+      <div className="font-bold text-2xl mb-4">Projects</div>
+      <div className="site-card-wrapper">
+        <Row gutter={[8, 24]}>
+          {filteredData.map((items, index) => {
+            return (
+              <Col lg={{ span: 24 }} key={index}>
+                <ProjectCard data={items} />
+              </Col>
+            )
+          })}
+        </Row>
+      </div>
     </LayoutDashboard>
   )
 }
