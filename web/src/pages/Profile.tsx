ea8af43bc799f7ac93b5ce837d08034a792024d6
Akoros <akoranos@gmail.com>
2020-08-13 15:32:14 +0700

    Update projects card design

--- web/src/pages/Profile.tsx
+++ web/src/pages/Profile.tsx
@@ -1,16 +1,18 @@
 import { CheckCircleOutlined, RightOutlined, SyncOutlined } from '@ant-design/icons'
-import { Button, Card, Col, Empty, Radio, Row, Tag, Typography } from 'antd'
+import { Avatar, Button, Card, Col, Empty, Radio, Row, Tag, Typography } from 'antd'
 import React from 'react'
 import { LayoutProfile } from '../components/DashboardComponent'
 
 function Profile() {
   const { Title, Text } = Typography
+  const { Meta } = Card
   const projects = [
     {
       id: '1',
       projectName: 'Project #1',
       projectType: 'Web design',
-      ProjectDetail: 'Commodo adipiscing ornare sit lorem sit tempus urna, vestibulum, neque.',
+      projectDetail: 'Commodo adipiscing ornare sit lorem sit tempus urna, vestibulum, neque.',
+      projectImage: 'https://source.unsplash.com/600x600/?work',
       status: 'undone',
       team: [
         {
@@ -34,7 +36,48 @@ function Profile() {
       id: '2',
       projectName: 'Project #2',
       projectType: 'Marketing',
-      ProjectDetail: 'Consequat tempus nisi, orci, ligula duis.',
+      projectDetail: 'Consequat tempus nisi, orci, ligula duis.',
+      projectImage: 'https://source.unsplash.com/600x600/?work',
+      status: 'done',
+      team: [
+        {
+          id: '3',
+          name: 'test 11',
+          image: 'https://source.unsplash.com/600x600/?people',
+        },
+        {
+          id: '4',
+          name: 'test 12',
+          image: 'https://source.unsplash.com/600x600/?people',
+        },
+      ],
+    },
+    {
+      id: '3',
+      projectName: 'Project #3',
+      projectType: 'Marketing',
+      projectDetail: 'Consequat tempus nisi, orci, ligula duis.',
+      projectImage: 'https://source.unsplash.com/600x600/?work',
+      status: 'done',
+      team: [
+        {
+          id: '3',
+          name: 'test 11',
+          image: 'https://source.unsplash.com/600x600/?people',
+        },
+        {
+          id: '4',
+          name: 'test 12',
+          image: 'https://source.unsplash.com/600x600/?people',
+        },
+      ],
+    },
+    {
+      id: '4',
+      projectName: 'Project #4',
+      projectType: 'Marketing',
+      projectDetail: 'Consequat tempus nisi, orci, ligula duis.',
+      projectImage: 'https://source.unsplash.com/600x600/?work',
       status: 'done',
       team: [
         {
@@ -68,7 +111,7 @@ function Profile() {
                   >
                     <Col className="flex flex-col">
                       <Text className="font-bold">{value.projectType}</Text>
-                      <Text>{value.ProjectDetail.substr(0, 100) + '...'}</Text>
+                      <Text>{value.projectDetail.substr(0, 100) + '...'}</Text>
                     </Col>
                     <Row className="mt-6">
                       <Col span={13}>
@@ -117,29 +160,58 @@ function Profile() {
               </Row>
             </Col>
             <Col>
-              <Radio.Group defaultValue="a" size="large">
-                <Radio.Button value="a">All</Radio.Button>
-                <Radio.Button value="b">In progress</Radio.Button>
-                <Radio.Button value="c">Closed</Radio.Button>
+              <Radio.Group defaultValue="all" size="large">
+                <Radio.Button value="all">All</Radio.Button>
+                <Radio.Button value="undone">In progress</Radio.Button>
+                <Radio.Button value="done">Closed</Radio.Button>
               </Radio.Group>
             </Col>
           </Row>
           <Row gutter={16}>
-            <Col span={8}>
-              <Card hoverable title="Project Name" bordered={false}>
-                Card content
-              </Card>
-            </Col>
-            <Col span={8}>
-              <Card hoverable title="Project Name" bordered={false}>
-                Card content
-              </Card>
-            </Col>
-            <Col span={8}>
-              <Card hoverable title="Project Name" bordered={false}>
-                Card content
-              </Card>
-            </Col>
+            {projects ? (
+              projects.map((value, index) => (
+                <Col span={8} key={index}>
+                  <Card hoverable bordered={false} className="mb-4">
+                    <Meta
+                      avatar={<Avatar size={60} src={value.projectImage} />}
+                      title={value.projectName}
+                      description={`${value.projectType} | ...Tasks`}
+                    />
+                    <Text className="font-bold">Member(s)</Text>
+                    <Row className="pl-16 mt-2">
+                      {value
+                        ? value.team.map((team, index) => (
+                            <Avatar key={index} src={team.image} className="ml-2" alt={team.name} />
+                          ))
+                        : ''}
+                    </Row>
+                    <Row className="flex items-end justify-end mt-6">
+                      <Col>
+                        {value.status === 'done' ? (
+                          <Tag
+                            color="green"
+                            className="rounded-full py-1 px-2"
+                            icon={<CheckCircleOutlined className="animate-bounce" />}
+                          >
+                            <Text className="font-bold">Success</Text>
+                          </Tag>
+                        ) : (
+                          <Tag
+                            color="gold"
+                            className="rounded-full py-1 px-2"
+                            icon={<SyncOutlined spin />}
+                          >
+                            <Text className="font-bold">In Progress</Text>
+                          </Tag>
+                        )}
+                      </Col>
+                    </Row>
+                  </Card>
+                </Col>
+              ))
+            ) : (
+              <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+            )}
           </Row>
         </Col>
       </LayoutProfile>
