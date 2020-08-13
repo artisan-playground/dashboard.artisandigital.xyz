86bbd143712922569f4978134db8a9b672c29767
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-13 17:58:54 +0700

    Sorted by type

---
+++ web/src/DATA.tsx
@@ -0,0 +1,129 @@
+export const DATA: any[] = [
+  {
+    id: '1',
+    projectName: 'Project #1',
+    projectType: 'Web design',
+    ProjectDetail: 'Commodo adipiscing ornare sit lorem sit tempus urna, vestibulum, neque.',
+    status: 'undone',
+    team: [
+      {
+        id: '1',
+        name: 'test 1',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+      {
+        id: '2',
+        name: 'test 2',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+      {
+        id: '3',
+        name: 'test 3',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+    ],
+  },
+  {
+    id: '2',
+    projectName: 'Project #2',
+    projectType: 'Marketing',
+    ProjectDetail: 'Consequat tempus nisi, orci, ligula duis.',
+    status: 'done',
+    team: [
+      {
+        id: '3',
+        name: 'test 11',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+      {
+        id: '4',
+        name: 'test 12',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+    ],
+  },
+  {
+    id: '3',
+    projectName: 'Project #3',
+    projectType: 'Mobile App',
+    ProjectDetail:
+      'Nisi lacus sed viverra tellus in hac. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida.',
+    status: 'done',
+    team: [
+      {
+        id: '5',
+        name: 'test 115',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+      {
+        id: '6',
+        name: 'test 162',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+    ],
+  },
+  {
+    id: '4',
+    projectName: 'Project #4',
+    projectType: 'Mobile App',
+    ProjectDetail:
+      'Urna nunc id cursus metus aliquam eleifend. Ultrices neque ornare aenean euismod elementum. Eget arcu dictum varius duis at consectetur',
+    status: 'done',
+    team: [
+      {
+        id: '7',
+        name: 'test 115',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+      {
+        id: '8',
+        name: 'test 162',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+      {
+        id: '9',
+        name: 'test 162',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+    ],
+  },
+  {
+    id: '5',
+    projectName: 'Project #5',
+    projectType: 'Mobile App',
+    ProjectDetail: 'Blandit volutpat maecenas volutpat blandit aliquam etiam erat.',
+    status: 'done',
+    team: [
+      {
+        id: '7',
+        name: 'test 115',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+      {
+        id: '8',
+        name: 'test 162',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+      {
+        id: '9',
+        name: 'test 162',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+    ],
+  },
+  {
+    id: '6',
+    projectName: 'Project #6',
+    projectType: 'Mobile App',
+    ProjectDetail:
+      'Euismod in pellentesque massa placerat duis. Elementum sagittis vitae et leo duis ut diam quam nulla. ',
+    status: 'undone',
+    team: [
+      {
+        id: '9',
+        name: 'test 162',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+    ],
+  },
+]
