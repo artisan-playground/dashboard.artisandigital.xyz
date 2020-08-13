a80f10d1a6d4db8c98196987235fcb54ff4456ee
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-13 15:09:27 +0700

    Fix params and query and side nav on selected

--- web/src/pages/ProjectDetail.tsx
+++ web/src/pages/ProjectDetail.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import { LayoutDashboard } from '../components/DashboardComponent'
 
 function ProjectDetail() {
+  const projectId: any = useParams()
+
   return (
     <LayoutDashboard>
-      <div>detail</div>
+      <div>detail of user {projectId.projectId}</div>
     </LayoutDashboard>
   )
 }
--- web/src/pages/ProjectList.tsx
+++ web/src/pages/ProjectList.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import { LayoutDashboard } from '../components/DashboardComponent'
 
-function ProjectList() {
+function ProjectList(props: any) {
+  const query = useQuery()
+
+  function useQuery() {
+    return new URLSearchParams(useLocation().search)
+  }
+
   return (
     <LayoutDashboard>
-      <div>Project list</div>
+      <div>Project list {query.get('types')}</div>
     </LayoutDashboard>
   )
 }
