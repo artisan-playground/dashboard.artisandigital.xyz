5a3f93cb17553f9ebec3f27ff023a6f5858e9122
Akoros <akoranos@gmail.com>
2020-08-14 16:44:14 +0700

    Update side profile

--- web/src/components/DashboardComponent.tsx
+++ web/src/components/DashboardComponent.tsx
@@ -2,8 +2,8 @@ import LayoutDashboard from '../components/LayoutDashboard'
 import LayoutProfile from '../components/LayoutProfile'
 import NavBar from '../components/NavBar'
 import ProjectCard from '../components/ProjectCard'
-import ProfileSide from '../components/ProfileSide'
 import SideNav from '../components/SideNav'
+import SideProfile from '../components/SideProfile'
 import WelcomeCard from '../components/WelcomeCard'
 
-export { NavBar, SideNav, LayoutDashboard, WelcomeCard, ProjectCard, LayoutProfile, ProfileSide }
+export { NavBar, SideNav, LayoutDashboard, WelcomeCard, ProjectCard, LayoutProfile, SideProfile }
