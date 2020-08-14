9d70c3003ad1940c449c8bb490174ceebd27e5c7
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-14 14:51:48 +0700

    Pass data to ProjectDetail page

--- web/src/pages/ProjectDetail.tsx
+++ web/src/pages/ProjectDetail.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import { LayoutDashboard } from '../components/DashboardComponent'
 
-function ProjectDetail() {
+function ProjectDetail(props: any) {
   const { projectId } = useParams()
+  const data = props.location.state.data
 
   return (
     <LayoutDashboard>
       <div>detail of project {projectId}</div>
+      <div>{data.projectName}</div>
     </LayoutDashboard>
   )
 }
