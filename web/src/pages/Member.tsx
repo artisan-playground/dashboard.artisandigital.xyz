8716692341588e0c4cf39e0c56b843f1b7feda43
Akoros <akoranos@gmail.com>
2020-08-17 17:33:04 +0700

    Update pagination table

--- web/src/pages/Member.tsx
+++ web/src/pages/Member.tsx
@@ -1,3 +1,4 @@
+import Avatar from 'antd/lib/avatar/avatar'
 import React from 'react'
 import { LayoutDashboard } from '../components/DashboardComponent'
 import MemberTable from '../components/MemberTable'
@@ -5,6 +6,10 @@ import { Sorter } from '../utils/Sorter'
 
 function Member() {
   const columns = [
+    {
+      dataIndex: 'image',
+      render: (image: any) => <Avatar src={image} />,
+    },
     {
       title: 'Name',
       dataIndex: 'name',
@@ -28,36 +33,41 @@ function Member() {
         compare: Sorter.DEFAULT,
         multiple: 1,
       },
-    }
+    },
   ]
 
   const data = [
     {
       key: '1',
+      image: 'https://source.unsplash.com/600x600/?people',
       name: 'John Brown',
       position: 'Developer',
       project: 2,
     },
     {
       key: '2',
+      image: 'https://source.unsplash.com/600x600/?people',
       name: 'Jim Green',
       position: 'Developer',
       project: 1,
     },
     {
       key: '3',
+      image: 'https://source.unsplash.com/600x600/?people',
       name: 'Joe Black',
       position: 'Developer',
       project: 3,
     },
     {
       key: '4',
+      image: 'https://source.unsplash.com/600x600/?people',
       name: 'Jim Red',
       position: 'Developer',
       project: 5,
     },
     {
       key: '5',
+      image: 'https://source.unsplash.com/600x600/?people',
       name: 'John Doe',
       position: 'Developer',
       project: 4,
