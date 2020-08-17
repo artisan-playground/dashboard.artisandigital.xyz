9f12f9b530be354e9e01ba756ac9eafd5915f8e9
Akoros <akoranos@gmail.com>
2020-08-17 15:54:12 +0700

    Update sort table

--- web/src/pages/Member.tsx
+++ web/src/pages/Member.tsx
@@ -1,21 +1,34 @@
 import React from 'react'
 import { LayoutDashboard } from '../components/DashboardComponent'
 import MemberTable from '../components/MemberTable'
+import { Sorter } from '../utils/Sorter'
 
 function Member() {
   const columns = [
     {
       title: 'Name',
       dataIndex: 'name',
+      sorter: {
+        compare: Sorter.DEFAULT,
+        multiple: 3,
+      },
     },
     {
       title: 'Job Position',
       dataIndex: 'position',
+      sorter: {
+        compare: Sorter.DEFAULT,
+        multiple: 2,
+      },
     },
     {
       title: 'Project(s)',
       dataIndex: 'project',
-    },
+      sorter: {
+        compare: Sorter.DEFAULT,
+        multiple: 1,
+      },
+    }
   ]
 
   const data = [
@@ -23,31 +36,31 @@ function Member() {
       key: '1',
       name: 'John Brown',
       position: 'Developer',
-      project: '2',
+      project: 2,
     },
     {
       key: '2',
       name: 'Jim Green',
       position: 'Developer',
-      project: '1',
+      project: 1,
     },
     {
       key: '3',
       name: 'Joe Black',
       position: 'Developer',
-      project: '3',
+      project: 3,
     },
     {
       key: '4',
       name: 'Jim Red',
       position: 'Developer',
-      project: '2',
+      project: 5,
     },
     {
       key: '5',
       name: 'John Doe',
       position: 'Developer',
-      project: '4',
+      project: 4,
     },
   ]
 
