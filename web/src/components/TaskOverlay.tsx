5a67694fe2ba4b6221356935471c5b7c5c113a33
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-18 11:45:26 +0700

    Add Done button

--- web/src/components/TaskOverlay.tsx
+++ web/src/components/TaskOverlay.tsx
@@ -1,4 +1,6 @@
 import {
+  BorderOutlined,
+  CheckSquareOutlined,
   ClockCircleOutlined,
   CloseCircleOutlined,
   CommentOutlined,
@@ -6,20 +8,34 @@ import {
   PaperClipOutlined,
   TeamOutlined,
 } from '@ant-design/icons'
-import { Col, Modal, Row, Typography } from 'antd'
+import { Button, Col, Modal, Row, Skeleton, Typography } from 'antd'
 import Avatar from 'antd/lib/avatar/avatar'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function TaskOverlay({ data, project, visible }: any) {
   const { Text } = Typography
 
-  function hideModal() {}
+  const [taskData, setTaskData]: any = useState({})
 
-  return (
+  useEffect(() => {
+    setTaskData(data)
+  }, [data])
+
+  function onDoneClick(event: any) {
+    // event.stopPropagation()
+    let tempData = { ...data }
+    setTaskData({ ...tempData, isDone: !tempData.isDone })
+  }
+
+  return Object.keys(taskData).length === 0 ? (
+    <Modal visible={visible}>
+      <Skeleton />
+    </Modal>
+  ) : (
     <Modal
       visible={visible}
-      onCancel={hideModal}
+      footer={null}
       bodyStyle={{
         borderRadius: 120,
         paddingTop: 56,
@@ -32,11 +48,13 @@ function TaskOverlay({ data, project, visible }: any) {
       width={'80%'}
       closeIcon={<CloseCircleOutlined style={{ color: 'red', fontSize: 24 }} />}
     >
-      <Text className="absolute top-0 left-0 text-2xl font-bold mt-4 ml-4">{data.taskName}</Text>
+      <Text className="absolute top-0 left-0 text-2xl font-bold mt-4 ml-4">
+        {taskData.taskName}
+      </Text>
       <Row className="py-4">
-        <Col span={24} lg={{ span: 18 }} className="pl-2 pr-2 pt-2 border-r-2">
+        <Col span={24} lg={{ span: 18 }} className="pl-4 pr-4 pt-2 border-r-2">
           <Row>
-            <Text className="text-lg pl-2 mb-2">{data.taskDetail}</Text>
+            <Text className="text-lg pl-2 mb-2">{taskData.taskDetail}</Text>
           </Row>
           <Row className="items-center">
             <PaperClipOutlined className="mr-2" style={{ color: '#105EFC', fontSize: 24 }} />
@@ -47,7 +65,7 @@ function TaskOverlay({ data, project, visible }: any) {
             <Text className="text-lg font-bold">Comment</Text>
           </Row>
         </Col>
-        <Col span={24} lg={{ span: 6 }} className="pl-2">
+        <Col span={24} lg={{ span: 6 }} className="pl-4">
           <Row className="items-center">
             <FundProjectionScreenOutlined
               className="mr-2"
@@ -70,9 +88,13 @@ function TaskOverlay({ data, project, visible }: any) {
             <Text className="text-lg font-bold">Team</Text>
           </Row>
           <Row className="ml-2">
-            {data.team.map((items: any) => {
+            {taskData.team.map((items: any) => {
               return (
-                <Link key={items.id} to={{ pathname: '/profile', state: { profileId: items.id } }}>
+                <Link
+                  className="w-full"
+                  key={items.id}
+                  to={{ pathname: '/profile', state: { profileId: items.id } }}
+                >
                   <div className="flex mx-0 my-1 p-2 rounded-lg hover:bg-primary hover:text-white cursor-pointer">
                     <Avatar key={items.id} src={items.image} alt={items.name} />
                     <div className="ml-4 text-lg">{items.name}</div>
@@ -80,6 +102,24 @@ function TaskOverlay({ data, project, visible }: any) {
                 </Link>
               )
             })}
+          </Row>
+          <Row className="justify-center">
+            <Button
+              className="flex items-center p-4 m-4"
+              type="primary"
+              danger={taskData.isDone ? true : false}
+              icon={
+                taskData.isDone ? (
+                  <BorderOutlined style={{ fontSize: 16 }} />
+                ) : (
+                  <CheckSquareOutlined style={{ fontSize: 16 }} />
+                )
+              }
+              shape="round"
+              onClick={onDoneClick}
+            >
+              {taskData.isDone ? 'Mark as Undone' : 'Mark as Done'}
+            </Button>
           </Row>
         </Col>
       </Row>
