97b12a4d1662164a97bbc707f2f9768716bb95fd
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-20 17:55:41 +0700

    Add new page

--- web/src/pages/News.tsx
+++ web/src/pages/News.tsx
@@ -1,32 +1,156 @@
-import { Card, Col, Row } from 'antd'
+import { Card, Col, Row, Typography } from 'antd'
+import Avatar from 'antd/lib/avatar/avatar'
 import React from 'react'
-import { LayoutDashboard } from '../components/DashboardComponent'
+import { EventCard, LayoutDashboard } from '../components/DashboardComponent'
 
 function News() {
+  const { Text } = Typography
+  const { Meta } = Card
+
+  const data = [
+    {
+      id: '1',
+      eventDate: new Date(),
+      eventName: 'Meeting #1',
+      note: 'In 2nd floor meeting room',
+      tag: 'Important',
+      invited: [
+        {
+          id: '1',
+          name: 'Lara',
+          image: 'https://source.unsplash.com/601x601/?user',
+        },
+        {
+          id: '2',
+          name: 'Pogba',
+          image: 'https://source.unsplash.com/602x601/?user',
+        },
+      ],
+    },
+    {
+      id: '2',
+      eventDate: new Date(),
+      eventName: 'Drinking',
+      note: 'Tha Chang Café',
+      tag: 'Important',
+      invited: [
+        {
+          id: '3',
+          name: 'Jaonaaw',
+          image: 'https://source.unsplash.com/603x601/?user',
+        },
+        {
+          id: '4',
+          name: 'Moew',
+          image: 'https://source.unsplash.com/604x601/?user',
+        },
+      ],
+    },
+  ]
   return (
     <LayoutDashboard noCard>
       <div className="font-bold text-2xl mb-4">Paragraph #1</div>
-
-      <Row className="w-full justify-center overflow-y-auto">
+      <Row className="w-full justify-center overflow-y-auto mb-4 p-2">
         <Col span={24} md={{ span: 7 }} className="mx-2 mb-2">
-          <Card className="w-full h-24 rounded-lg">1</Card>
+          <Card
+            className="w-full rounded-lg shadow-lg"
+            cover={<img src="https://source.unsplash.com/600x600/?cat" alt="kityy1" />}
+          >
+            <Meta
+              avatar={<Avatar src="https://source.unsplash.com/600x600/?user" />}
+              title="Kitty 1"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua."
+            />
+            <Row className="justify-between mt-4 pl-12">
+              <Col span={24} xl={{ span: 12 }}>
+                <Text disabled>{new Date().toLocaleDateString()}</Text>
+              </Col>
+              <Col span={24} xl={{ span: 12 }}>
+                <a href="/news">Read more</a>
+              </Col>
+            </Row>
+          </Card>
         </Col>
         <Col span={24} md={{ span: 7 }} className="mx-2 mb-2">
-          <Card className="w-full h-24 rounded-lg">2</Card>
+          <Card
+            className="w-full rounded-lg shadow-lg"
+            cover={<img src="https://source.unsplash.com/600x601/?cat" alt="kityy2" />}
+          >
+            <Meta
+              avatar={<Avatar src="https://source.unsplash.com/600x601/?user" />}
+              title="Kitty 2"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua."
+            />
+            <Row className="justify-between mt-4 pl-12">
+              <Col span={24} xl={{ span: 12 }}>
+                <Text disabled>{new Date().toLocaleDateString()}</Text>
+              </Col>
+              <Col span={24} xl={{ span: 12 }}>
+                <a href="/news">Read more</a>
+              </Col>
+            </Row>
+          </Card>
         </Col>
         <Col span={24} md={{ span: 7 }} className="mx-2 mb-2">
-          <Card className="w-full h-24 rounded-lg">3</Card>
+          <Card
+            className="w-full rounded-lg shadow-lg"
+            cover={<img src="https://source.unsplash.com/601x600/?cat" alt="kityy1" />}
+          >
+            <Meta
+              avatar={<Avatar src="https://source.unsplash.com/601x600/?user" />}
+              title="Kitty 3"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua."
+            />
+            <Row className="justify-between mt-4 pl-12">
+              <Col span={24} xl={{ span: 12 }}>
+                <Text disabled>{new Date().toLocaleDateString()}</Text>
+              </Col>
+              <Col span={24} xl={{ span: 12 }}>
+                <a href="/news">Read more</a>
+              </Col>
+            </Row>
+          </Card>
         </Col>
       </Row>
+
       <div className="font-bold text-2xl mb-4">Paragraph #2</div>
-
-      <Row>
-        <Card></Card>
+      <Row className="w-full  overflow-y-auto mb-4 p-4">
+        <Card className="w-full rounded-lg shadow-lg">
+          <Row>
+            <Col span={24} md={{ span: 10 }}>
+              <img
+                src="https://source.unsplash.com/600x600/?seal"
+                alt="kityy1"
+                className="rounded-lg"
+              />
+            </Col>
+            <Col span={24} md={{ span: 10 }} className="p-4 ml-4">
+              <Text className="font-bold text-lg">News #1</Text>
+              <br />
+              <Text>
+                Vitae auctor eu augue ut lectus arcu bibendum at varius. Ultricies lacus sed turpis
+                tincidunt id aliquet risus feugiat in. Pharetra massa massa ultricies mi quis
+                hendrerit dolor magna. Leo duis ut diam quam nulla. Commodo sed egestas egestas
+                fringilla phasellus faucibus scelerisque. Gravida neque convallis a cras semper
+                auctor neque. Viverra orci sagittis eu volutpat odio facilisis mauris sit. Justo
+                donec enim diam vulputate. Pellentesque pulvinar pellentesque habitant morbi.
+                Bibendum arcu vitae elementum curabitur vitae. Sapien nec sagittis aliquam malesuada
+                bibendum arcu vitae elementum. Risus sed vulputate odio ut enim. Habitant morbi
+                tristique senectus et netus et malesuada fames ac. Nec ullamcorper sit amet risus.
+                Nam libero justo laoreet sit amet cursus sit amet dictum. Facilisi cras fermentum
+                odio eu feugiat pretium nibh ipsum consequat.
+              </Text>
+            </Col>
+          </Row>
+        </Card>
       </Row>
-      <div className="font-bold text-2xl mb-4">Paragraph #3</div>
 
-      <Row>
-        <Card></Card>
+      <div className="font-bold text-2xl mb-4">Event</div>
+      <Row className="w-full  overflow-y-auto mb-4 p-4">
+        <EventCard data={data} />
       </Row>
     </LayoutDashboard>
   )
