8dc777cd95e336351bcba60c2c39d1d9b0516315
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-20 17:55:41 +0700

    Add new page

--- web/src/components/DashboardComponent.tsx
+++ web/src/components/DashboardComponent.tsx
@@ -1,9 +1,18 @@
+import EventCard from '../components/EventCard'
 import LayoutDashboard from '../components/LayoutDashboard'
 import LayoutProfile from '../components/LayoutProfile'
 import NavBar from '../components/NavBar'
-import ProjectCard from '../components/ProjectCard'
 import ProfileSide from '../components/ProfileSide'
+import ProjectCard from '../components/ProjectCard'
 import SideNav from '../components/SideNav'
 import WelcomeCard from '../components/WelcomeCard'
-
-export { NavBar, SideNav, LayoutDashboard, WelcomeCard, ProjectCard, LayoutProfile, ProfileSide }
+export {
+  EventCard,
+  NavBar,
+  SideNav,
+  LayoutDashboard,
+  WelcomeCard,
+  ProjectCard,
+  LayoutProfile,
+  ProfileSide,
+}
