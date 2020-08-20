590e2dc88a930179f869a021d3e28974dd751fa5
Nhow <50292816+supreme5678@users.noreply.github.com>
2020-08-20 12:08:02 +0700

    Project (#18)
    * Sorted by type
    
    * Config project list layout
    
    * Pass data to ProjectDetail page
    
    * Create TaskCard LogCard & ProjectCard
    
    * Create Log card and Edit Task card lauout
    
    * Edit mock data
    
    * Update Project detail page
    
    * Add due date to project
    
    * Add TaskOverlay
    
    * Add Done button
    
    * Add carousel and Fix Overlay bug
    
    * Update Overlay
    
    * Positioning Overlay
    
    * Add comment section
    
    * Fix Clipboards and Comments
    
    * Remove project status on Nav
    
    * Add Search by Key and Tag
    
    * Change object to typescript
    
    * Fix comment bug
    
    * Remove log and one-line function
    
    * Remove return on map function
    
    * Sorted by type
    
    * Config project list layout
    
    * Pass data to ProjectDetail page
    
    * Create TaskCard LogCard & ProjectCard
    
    * Create Log card and Edit Task card lauout
    
    * Edit mock data
    
    * Update Project detail page
    
    * Add due date to project
    
    * Add TaskOverlay
    
    * Add Done button
    
    * Add carousel and Fix Overlay bug
    
    * Update Overlay
    
    * Positioning Overlay
    
    * Add comment section
    
    * Fix Clipboards and Comments
    
    * Remove project status on Nav
    
    * Add Search by Key and Tag
    
    * Change object to typescript
    
    * Fix comment bug
    
    * Remove log and one-line function
    
    * Remove return on map function
    
    * Sorted by type
    
    * Config project list layout
    
    * Pass data to ProjectDetail page
    
    * Create TaskCard LogCard & ProjectCard
    
    * Create Log card and Edit Task card lauout
    
    * Edit mock data
    
    * Update Project detail page
    
    * Add due date to project
    
    * Add TaskOverlay
    
    * Add Done button
    
    * Add carousel and Fix Overlay bug
    
    * Update Overlay
    
    * Positioning Overlay
    
    * Add comment section
    
    * Fix Clipboards and Comments
    
    * Remove project status on Nav
    
    * Add Search by Key and Tag
    
    * Change object to typescript
    
    * Fix comment bug
    
    * Remove log and one-line function
    
    * Remove return on map function
    
    * Sorted by type
    
    * Add Done button
    
    * Add carousel and Fix Overlay bug
    
    * Update Overlay
    
    * Fix Clipboards and Comments
    
    * Add Search by Key and Tag
    
    * Change object to typescript
    
    * Fix ProjectList bug

---
+++ web/src/DATA.tsx
@@ -0,0 +1,518 @@
+export const DATA: any[] = [
+  {
+    id: '1',
+    projectName: 'Project #1',
+    projectType: 'Web design',
+    projectDetail: 'Commodo adipiscing ornare sit lorem sit tempus urna, vestibulum, neque.',
+    status: 'undone',
+    dueDate: new Date(),
+    team: [
+      {
+        id: '1',
+        name: 'John 1',
+        image: 'https://source.unsplash.com/700x700/?people',
+      },
+      {
+        id: '2',
+        name: 'Jane 2',
+        image: 'https://source.unsplash.com/800x600/?people',
+      },
+      {
+        id: '3',
+        name: 'Jane 3',
+        image: 'https://source.unsplash.com/600x900/?people',
+      },
+    ],
+  },
+  {
+    id: '2',
+    projectName: 'Project #2',
+    projectType: 'Marketing',
+    projectDetail: 'Consequat tempus nisi, orci, ligula duis.',
+    status: 'done',
+    dueDate: new Date(),
+    team: [
+      {
+        id: '3',
+        name: 'Jane 11',
+        image: 'https://source.unsplash.com/600x900/?people',
+      },
+      {
+        id: '4',
+        name: 'Josh 12',
+        image: 'https://source.unsplash.com/900x900/?people',
+      },
+      {
+        id: '4',
+        name: 'Joshure 12',
+        image: 'https://source.unsplash.com/600x800/?people',
+      },
+      {
+        id: '4',
+        name: 'Jake 12',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+      {
+        id: '4',
+        name: 'Jaka 12',
+        image: 'https://source.unsplash.com/400x600/?people',
+      },
+      {
+        id: '4',
+        name: 'Jaki 12',
+        image: 'https://source.unsplash.com/600x400/?people',
+      },
+
+      {
+        id: '4',
+        name: 'Jeki 12',
+        image: 'https://source.unsplash.com/600x601/?people',
+      },
+      {
+        id: '4',
+        name: 'Jeko 12',
+        image: 'https://source.unsplash.com/600x602/?people',
+      },
+      {
+        id: '4',
+        name: 'Jeky 12',
+        image: 'https://source.unsplash.com/600x603/?people',
+      },
+      {
+        id: '4',
+        name: 'Jekii 12',
+        image: 'https://source.unsplash.com/600x604/?people',
+      },
+      {
+        id: '4',
+        name: 'Jikii 12',
+        image: 'https://source.unsplash.com/600x605/?people',
+      },
+
+      {
+        id: '4',
+        name: 'Jiko 12',
+        image: 'https://source.unsplash.com/600x606/?people',
+      },
+      {
+        id: '4',
+        name: 'Jika 12',
+        image: 'https://source.unsplash.com/600x607/?people',
+      },
+    ],
+  },
+  {
+    id: '3',
+    projectName: 'Project #3',
+    projectType: 'Mobile App',
+    projectDetail:
+      'Nisi lacus sed viverra tellus in hac. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida.',
+    status: 'done',
+    dueDate: new Date(),
+    team: [
+      {
+        id: '5',
+        name: 'Jike 115',
+        image: 'https://source.unsplash.com/600x608/?people',
+      },
+      {
+        id: '6',
+        name: 'Jiku Doe',
+        image: 'https://source.unsplash.com/600x609/?people',
+      },
+    ],
+  },
+  {
+    id: '4',
+    projectName: 'Project #4',
+    projectType: 'Mobile App',
+    projectDetail:
+      'Urna nunc id cursus metus aliquam eleifend. Ultrices neque ornare aenean euismod elementum. Eget arcu dictum varius duis at consectetur',
+    status: 'done',
+    dueDate: new Date(),
+    team: [
+      {
+        id: '7',
+        name: 'Juku 115',
+        image: 'https://source.unsplash.com/601x600/?people',
+      },
+      {
+        id: '8',
+        name: 'Juko Doe',
+        image: 'https://source.unsplash.com/602x600/?people',
+      },
+      {
+        id: '9',
+        name: 'Juki Doe',
+        image: 'https://source.unsplash.com/603x600/?people',
+      },
+    ],
+  },
+  {
+    id: '5',
+    projectName: 'Project #5',
+    projectType: 'Mobile App',
+    projectDetail: 'Blandit volutpat maecenas volutpat blandit aliquam etiam erat.',
+    status: 'done',
+    dueDate: new Date(),
+    team: [
+      {
+        id: '7',
+        name: 'Juki 115',
+        image: 'https://source.unsplash.com/604x600/?people',
+      },
+      {
+        id: '8',
+        name: 'Juke Doe',
+        image: 'https://source.unsplash.com/605x600/?people',
+      },
+      {
+        id: '9',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/606x600/?people',
+      },
+      {
+        id: '99',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/606x601/?people',
+      },
+      {
+        id: '999',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/606x602/?people',
+      },
+      {
+        id: '29',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/606x603/?people',
+      },
+    ],
+  },
+  {
+    id: '6',
+    projectName: 'Project #6',
+    projectType: 'Mobile App',
+    projectDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis posuere morbi leo urna molestie at elementum. Vitae congue mauris rhoncus aenean vel. Scelerisque purus semper eget duis at tellus at urna. Venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin. Dui accumsan sit amet nulla facilisi morbi tempus. Turpis massa tincidunt dui ut ornare lectus sit amet est. Quis imperdiet massa tincidunt nunc pulvinar sapien et. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt. Suspendisse interdum consectetur libero id faucibus nisl. Gravida neque convallis a cras semper auctor neque. In eu mi bibendum neque egestas congue quisque egestas. Pharetra diam sit amet nisl suscipit adipiscing bibendum est. Metus dictum at tempor commodo ullamcorper a. Eget nulla facilisi etiam dignissim diam quis. Blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque. A iaculis at erat pellentesque adipiscing. Pellentesque massa placerat duis ultricies lacus sed. Phasellus egestas tellus rutrum tellus.',
+    status: 'undone',
+    dueDate: new Date(),
+    team: [
+      {
+        id: '9',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/607x600/?people',
+      },
+    ],
+  },
+]
+
+export const TASK_DATA: any[] = [
+  {
+    id: '1',
+    projectId: '1',
+    taskName: 'Design Webpage',
+    time: new Date(),
+    taskDetail:
+      'Sit amet mattis vulputate enim nulla aliquet porttitor lacus. Aliquam sem fringilla ut morbi. Arcu non odio euismod lacinia at quis risus sed. Varius duis at consectetur lorem donec massa. A arcu cursus vitae congue mauris rhoncus.',
+    isDone: false,
+    team: [
+      {
+        id: '9',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/607x600/?people',
+      },
+      {
+        id: '99',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/607x609/?people',
+      },
+      {
+        id: '19',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/608x600/?people',
+      },
+      {
+        id: '29',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/607x601/?people',
+      },
+    ],
+    files: [
+      {
+        id: '1',
+        file: 'https://source.unsplash.com/607x601/?website',
+      },
+      {
+        id: '2',
+        file: 'https://source.unsplash.com/601x601/?website',
+      },
+      {
+        id: '3',
+        file: 'https://source.unsplash.com/601x602/?website',
+      },
+      {
+        id: '4',
+        file: 'https://source.unsplash.com/602x601/?design',
+      },
+      {
+        id: '5',
+        file: '',
+      },
+      {
+        id: '2',
+        file: 'https://source.unsplash.com/601x601/?website',
+      },
+      {
+        id: '3',
+        file: 'https://source.unsplash.com/601x602/?website',
+      },
+      {
+        id: '4',
+        file: 'https://source.unsplash.com/602x601/?design',
+      },
+      {
+        id: '5',
+        file: '',
+      },
+    ],
+    comments: [
+      {
+        id: '1',
+        timestamp: new Date('August 17, 2020 08:00:00'),
+        userId: '1',
+        image: '',
+        userImg: 'https://source.unsplash.com/607x601/?people',
+        userName: 'Alicia ki',
+        message: "I'll do Login page",
+      },
+      {
+        id: '2',
+        timestamp: new Date('August 17, 2020 08:30:00'),
+        userId: '1',
+        image: '',
+        userImg: 'https://source.unsplash.com/608x601/?people',
+        userName: 'Thomas train',
+        message: 'OK, lets go dude',
+      },
+      {
+        id: '3',
+        timestamp: new Date('August 17, 2020 09:00:00'),
+        userId: '12',
+        image: '',
+        userImg: 'https://source.unsplash.com/607x599/?people',
+        userName: 'Salmon train',
+        message: 'OK, lets go dude',
+      },
+      {
+        id: '4',
+        timestamp: new Date('August 17, 2020 09:45:00'),
+        userId: '22',
+        image: '',
+        userImg: 'https://source.unsplash.com/607x598/?people',
+        userName: 'Sashimi eiei',
+        message: 'OK, lets go dude',
+      },
+      {
+        id: '5',
+        timestamp: new Date('August 17, 2020 10:00:00'),
+        userId: '32',
+        image: '',
+        userImg: 'https://source.unsplash.com/607x602/?people',
+        userName: 'Doble black',
+        message: 'OK, lets go dude',
+      },
+      {
+        id: '6',
+        timestamp: new Date('August 17, 2020 10:21:00'),
+        userId: '33',
+        image: '',
+        userImg: 'https://source.unsplash.com/609x601/?people',
+        userName: 'Daisy Doaas',
+        message: 'OK, lets go dude',
+      },
+    ],
+  },
+  {
+    id: '2',
+    projectId: '1',
+    taskName: 'Implement code',
+    time: new Date(),
+    taskDetail: 'Odio ut sem nulla pharetra. Pharetra et ultrices neque ornare aenean.',
+    isDone: true,
+    team: [
+      {
+        id: '9',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/607x600/?people',
+      },
+      {
+        id: '90',
+        name: 'Dasisy Dsaoe',
+        image: 'https://source.unsplash.com/607x609/?people',
+      },
+      {
+        id: '21',
+        name: 'Arossma krif',
+        image: 'https://source.unsplash.com/607x599/?people',
+      },
+      {
+        id: '31',
+        name: 'Korn lek',
+        image: 'https://source.unsplash.com/599x599/?people',
+      },
+      {
+        id: '234',
+        name: 'Killua',
+        image: 'https://source.unsplash.com/598x599/?people',
+      },
+      {
+        id: '23w4',
+        name: 'Giant',
+        image: 'https://source.unsplash.com/598x598/?people',
+      },
+      {
+        id: '2384',
+        name: 'Nobita',
+        image: 'https://source.unsplash.com/597x598/?people',
+      },
+      {
+        id: '22w4',
+        name: 'Doraemon',
+        image: 'https://source.unsplash.com/598x597/?people',
+      },
+      {
+        id: '23w43',
+        name: 'Tsuneo',
+        image: 'https://source.unsplash.com/596x598/?people',
+      },
+    ],
+    files: [],
+    comments: [],
+  },
+  {
+    id: '3',
+    projectId: '2',
+    taskName: '#1',
+    time: new Date(),
+    taskDetail: 'Odio ut sem nulla pharetra. Pharetra et ultrices neque ornare aenean.',
+    isDone: true,
+    team: [
+      {
+        id: '9',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/607x600/?people',
+      },
+    ],
+    files: [],
+    comments: [],
+  },
+  {
+    id: '4',
+    projectId: '2',
+    taskName: '#2',
+    time: new Date(),
+    taskDetail: 'Odio ut sem nulla pharetra. Pharetra et ultrices neque ornare aenean.',
+    isDone: true,
+    team: [
+      {
+        id: '9',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/607x600/?people',
+      },
+    ],
+    files: [],
+    comments: [],
+  },
+  {
+    id: '5',
+    projectId: '2',
+    taskName: '#3',
+    time: new Date(),
+    taskDetail: 'Odio ut sem nulla pharetra. Pharetra et ultrices neque ornare aenean.',
+    isDone: false,
+    team: [
+      {
+        id: '9',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/607x600/?people',
+      },
+    ],
+    files: [],
+    comments: [],
+  },
+  {
+    id: '6',
+    projectId: '2',
+    taskName: '#4',
+    time: new Date(),
+    taskDetail: 'Odio ut sem nulla pharetra. Pharetra et ultrices neque ornare aenean.',
+    isDone: false,
+    team: [
+      {
+        id: '9',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/607x600/?people',
+      },
+    ],
+    files: [],
+    comments: [],
+  },
+  {
+    id: '7',
+    projectId: '2',
+    taskName: '#5',
+    time: new Date(),
+    taskDetail: 'Odio ut sem nulla pharetra. Pharetra et ultrices neque ornare aenean.',
+    isDone: true,
+    team: [
+      {
+        id: '9',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/607x600/?people',
+      },
+    ],
+    files: [],
+    comments: [],
+  },
+  {
+    id: '8',
+    projectId: '3',
+    taskName: 'Sunflower',
+    time: new Date(),
+    taskDetail:
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    isDone: true,
+    team: [
+      {
+        id: '9',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/607x600/?people',
+      },
+      {
+        id: '10',
+        name: 'Jane Doe',
+        image: 'https://source.unsplash.com/607x600/?people',
+      },
+    ],
+    files: [],
+    comments: [],
+  },
+  {
+    id: '9',
+    projectId: '4',
+    taskName: 'Proin nibh nisl condimentum id venenatis a condimentum vitae.',
+    time: new Date(),
+    taskDetail:
+      'Lobortis feugiat vivamus at augue eget. Sed faucibus turpis in eu mi bibendum. A arcu cursus vitae congue. Nec tincidunt praesent semper feugiat nibh sed pulvinar proin.',
+    isDone: false,
+    team: [
+      {
+        id: '9',
+        name: 'John Doe',
+        image: 'https://source.unsplash.com/607x600/?people',
+      },
+    ],
+    files: [],
+    comments: [],
+  },
+]
---
+++ web/src/components/TaskCard.tsx
@@ -0,0 +1,115 @@
+import {
+  BorderOutlined,
+  CheckSquareOutlined,
+  CommentOutlined,
+  PaperClipOutlined,
+} from '@ant-design/icons'
+import { Avatar, Button, Card, Col, Row, Skeleton, Tooltip, Typography } from 'antd'
+import React, { useEffect, useState } from 'react'
+import TaskOverlay from './TaskOverlay'
+
+function TaskCard({ data, project }: any) {
+  const { Text } = Typography
+
+  const [modalVisible, setModalVisible] = useState(false)
+  const [taskData, setTaskData]: any = useState({})
+
+  useEffect(() => {
+    setTaskData(data)
+  }, [data])
+
+  function toggleModal() {
+    setModalVisible(false)
+  }
+
+  function openModal() {
+    setModalVisible(true)
+  }
+
+  function onDoneClick(event: any) {
+    event.stopPropagation()
+
+    let tempData = { ...taskData }
+    setTaskData({ ...tempData, isDone: !tempData.isDone })
+  }
+
+  return !taskData && !data ? (
+    <Card>
+      <Skeleton />
+    </Card>
+  ) : (
+    <Card className="w-full rounded-lg shadow-md px-2 py-1 mb-4 cursor-pointer" onClick={openModal}>
+      <TaskOverlay
+        onCloseModal={toggleModal}
+        visible={modalVisible}
+        data={taskData}
+        project={project}
+      />
+      <Row gutter={[8, 16]}>
+        <div className="flex flex-col col-12 items-start justify-center">
+          <div className="flex flex-row items-center">
+            <Text className="font-bold text-xl">{data.taskName}</Text>
+            {data.files.length !== 0 ? (
+              <PaperClipOutlined className="ml-2 text-gray-500" />
+            ) : (
+              <div />
+            )}
+            {data.comments.length !== 0 ? (
+              <CommentOutlined className="ml-2 text-gray-500" />
+            ) : (
+              <div />
+            )}
+          </div>
+          <div className="flex flex-row">
+            <Text disabled>{data.time.toLocaleTimeString() || '...'}</Text>
+          </div>
+        </div>
+
+        <Col span={24}>
+          <Text className="text-lg">{data.taskDetail}</Text>
+        </Col>
+        <Col span={24}>
+          <Row className="justify-between">
+            <Col className=" min-h-full overflow-y-scroll overflow-x-auto">
+              <Avatar.Group
+                maxCount={2}
+                maxStyle={{ color: '#fff', backgroundColor: '#222', filter: 'brightness(0.6)' }}
+              >
+                {data.team.map((item: any) => (
+                  <Tooltip title={item.name} placement="top">
+                    <Avatar key={item.id} src={item.image} alt={item.name}></Avatar>
+                  </Tooltip>
+                ))}
+              </Avatar.Group>
+            </Col>
+            <Col className="absolute bottom-0 right-0 flex items-center justify-end">
+              {taskData.isDone ? (
+                <Button
+                  className="flex items-center justify-center"
+                  type="text"
+                  shape="circle"
+                  size="large"
+                  onClick={onDoneClick}
+                >
+                  <CheckSquareOutlined style={{ fontSize: 24 }} />
+                </Button>
+              ) : (
+                <Button
+                  className="flex items-center justify-center"
+                  type="text"
+                  shape="circle"
+                  size="large"
+                  onClick={onDoneClick}
+                >
+                  <BorderOutlined style={{ fontSize: 24 }} />
+                </Button>
+              )}
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Card>
+  )
+}
+
+export default TaskCard
