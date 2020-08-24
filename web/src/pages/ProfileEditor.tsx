d66dcffe93da4f3f4277e637c65ae63a809ab2a5
Akoros <akoranos@gmail.com>
2020-08-24 13:12:04 +0700

    Add preview image

--- web/src/pages/ProfileEditor.tsx
+++ web/src/pages/ProfileEditor.tsx
@@ -1,12 +1,13 @@
 import { LoadingOutlined, PlusOutlined } from '@ant-design/icons'
 import '@pathofdev/react-tag-input/build/index.css'
 import { Card, Col, Row, Upload } from 'antd'
+import ImgCrop from 'antd-img-crop'
 import React, { useState } from 'react'
 import { LayoutDashboard, ProfileForm, ProfileSkillTags } from '../components/DashboardComponent'
 import { USER_DATA } from '../DATA'
 
 function ProfileEditor() {
-  const [imageUrl, setImageUrl] = useState()
+  const [, setImageUrl] = useState('')
   const [loading, setLoading] = useState(false)
 
   function getBase64(img: any, callback: any) {
@@ -40,34 +41,52 @@ function ProfileEditor() {
     }
   }
 
+  const onPreview = async (file: any) => {
+    let src = file.url
+    if (!src) {
+      src = await new Promise((resolve) => {
+        const reader = new FileReader()
+        reader.readAsDataURL(file.originFileObj)
+        reader.onload = () => resolve(reader.result)
+      })
+    }
+    const image = new Image()
+    image.src = src
+    window.open(src)
+    src.document.write(image.outerHTML)
+  }
+
   return (
     <LayoutDashboard>
       <Row>
-        <Col xs={24} xl={6} className="mr-12 mb-4">
-          <Card className="flex items-center justify-center text-center mb-4">
-            <Upload
-              name="avatar"
-              listType="picture-card"
-              className="avatar-uploader"
-              showUploadList={false}
-              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-              beforeUpload={beforeUpload}
-              onChange={handleChange}
-            >
-              {imageUrl ? (
-                <img src={imageUrl} alt="avatar" style={{ width: '100%' }} />
-              ) : (
-                <div>
-                  {loading ? <LoadingOutlined /> : <PlusOutlined />}
-                  <div className="ant-upload-text">Upload</div>
-                </div>
-              )}
-            </Upload>
-          </Card>
-          {USER_DATA.map((items, index) => (
+        {USER_DATA.map((items, index) => (
+          <Col xs={24} xl={6} className="mr-12 mb-4">
+            <Card className="flex items-center justify-center text-center mb-4">
+              <ImgCrop rotate>
+                <Upload
+                  name="avatar"
+                  listType="picture-card"
+                  className="avatar-uploader"
+                  showUploadList={false}
+                  action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                  beforeUpload={beforeUpload}
+                  onChange={handleChange}
+                  onPreview={onPreview}
+                >
+                  {items ? (
+                    <img src={items.image} alt="avatar" style={{ width: '100%' }} />
+                  ) : (
+                    <div>
+                      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+                      <div className="ant-upload-text">Upload</div>
+                    </div>
+                  )}
+                </Upload>
+              </ImgCrop>
+            </Card>
             <ProfileSkillTags data={items} key={index} />
-          ))}
-        </Col>
+          </Col>
+        ))}
 
         <Col xs={24} xl={17}>
           <Card title="Edit Profile" headStyle={{ fontWeight: 'bold' }}>
