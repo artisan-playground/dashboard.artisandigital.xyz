46932568989586aabd589d2189ac04d8ca4e0a68
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-25 14:49:19 +0700

    Add Mock data

--- api/src/db/projectdata.ts
+++ api/src/db/projectdata.ts
@@ -1,5 +1,207 @@
 export const projects = [
-  { id: '1', name: 'Project 1', team: [{ id: '1', name: 'User 1' }] },
-  { id: '2', name: 'Project 2' },
-  { id: '3', name: 'Project 3', team: [{ id: '1', name: 'User 1' }] },
+  {
+    id: '1',
+    projectName: 'Project #1',
+    projectType: 'Web design',
+    projectDetail: 'Commodo adipiscing ornare sit lorem sit tempus urna, vestibulum, neque.',
+    status: 'undone',
+    dueDate: new Date(),
+    team: [
+      {
+        id: '1',
+        name: 'John 1',
+        image: 'https://source.unsplash.com/700x700/?people',
+      },
+      {
+        id: '2',
+        name: 'Jane 2',
+        image: 'https://source.unsplash.com/800x600/?people',
+      },
+      {
+        id: '3',
+        name: 'Jane 3',
+        image: 'https://source.unsplash.com/600x900/?people',
+      },
+    ],
+  },
+  {
+    id: '2',
+    projectName: 'Project #2',
+    projectType: 'Marketing',
+    projectDetail: 'Consequat tempus nisi, orci, ligula duis.',
+    status: 'done',
+    dueDate: new Date(),
+    team: [
+      {
+        id: '3',
+        name: 'Jane 11',
+        image: 'https://source.unsplash.com/600x900/?people',
+      },
+      {
+        id: '4',
+        name: 'Josh 12',
+        image: 'https://source.unsplash.com/900x900/?people',
+      },
+      {
+        id: '4',
+        name: 'Joshure 12',
+        image: 'https://source.unsplash.com/600x800/?people',
+      },
+      {
+        id: '4',
+        name: 'Jake 12',
+        image: 'https://source.unsplash.com/600x600/?people',
+      },
+      {
+        id: '4',
+        name: 'Jaka 12',
+        image: 'https://source.unsplash.com/400x600/?people',
+      },
+      {
+        id: '4',
+        name: 'Jaki 12',
+        image: 'https://source.unsplash.com/600x400/?people',
+      },
+
+      {
+        id: '4',
+        name: 'Jeki 12',
+        image: 'https://source.unsplash.com/600x601/?people',
+      },
+      {
+        id: '4',
+        name: 'Jeko 12',
+        image: 'https://source.unsplash.com/600x602/?people',
+      },
+      {
+        id: '4',
+        name: 'Jeky 12',
+        image: 'https://source.unsplash.com/600x603/?people',
+      },
+      {
+        id: '4',
+        name: 'Jekii 12',
+        image: 'https://source.unsplash.com/600x604/?people',
+      },
+      {
+        id: '4',
+        name: 'Jikii 12',
+        image: 'https://source.unsplash.com/600x605/?people',
+      },
+
+      {
+        id: '4',
+        name: 'Jiko 12',
+        image: 'https://source.unsplash.com/600x606/?people',
+      },
+      {
+        id: '4',
+        name: 'Jika 12',
+        image: 'https://source.unsplash.com/600x607/?people',
+      },
+    ],
+  },
+  {
+    id: '3',
+    projectName: 'Project #3',
+    projectType: 'Mobile App',
+    projectDetail:
+      'Nisi lacus sed viverra tellus in hac. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida.',
+    status: 'done',
+    dueDate: new Date(),
+    team: [
+      {
+        id: '5',
+        name: 'Jike 115',
+        image: 'https://source.unsplash.com/600x608/?people',
+      },
+      {
+        id: '6',
+        name: 'Jiku Doe',
+        image: 'https://source.unsplash.com/600x609/?people',
+      },
+    ],
+  },
+  {
+    id: '4',
+    projectName: 'Project #4',
+    projectType: 'Mobile App',
+    projectDetail:
+      'Urna nunc id cursus metus aliquam eleifend. Ultrices neque ornare aenean euismod elementum. Eget arcu dictum varius duis at consectetur',
+    status: 'done',
+    dueDate: new Date(),
+    team: [
+      {
+        id: '7',
+        name: 'Juku 115',
+        image: 'https://source.unsplash.com/601x600/?people',
+      },
+      {
+        id: '8',
+        name: 'Juko Doe',
+        image: 'https://source.unsplash.com/602x600/?people',
+      },
+      {
+        id: '9',
+        name: 'Juki Doe',
+        image: 'https://source.unsplash.com/603x600/?people',
+      },
+    ],
+  },
+  {
+    id: '5',
+    projectName: 'Project #5',
+    projectType: 'Mobile App',
+    projectDetail: 'Blandit volutpat maecenas volutpat blandit aliquam etiam erat.',
+    status: 'done',
+    dueDate: new Date(),
+    team: [
+      {
+        id: '7',
+        name: 'Juki 115',
+        image: 'https://source.unsplash.com/604x600/?people',
+      },
+      {
+        id: '8',
+        name: 'Juke Doe',
+        image: 'https://source.unsplash.com/605x600/?people',
+      },
+      {
+        id: '9',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/606x600/?people',
+      },
+      {
+        id: '99',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/606x601/?people',
+      },
+      {
+        id: '999',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/606x602/?people',
+      },
+      {
+        id: '29',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/606x603/?people',
+      },
+    ],
+  },
+  {
+    id: '6',
+    projectName: 'Project #6',
+    projectType: 'Mobile App',
+    projectDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis posuere morbi leo urna molestie at elementum. Vitae congue mauris rhoncus aenean vel. Scelerisque purus semper eget duis at tellus at urna. Venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin. Dui accumsan sit amet nulla facilisi morbi tempus. Turpis massa tincidunt dui ut ornare lectus sit amet est. Quis imperdiet massa tincidunt nunc pulvinar sapien et. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt. Suspendisse interdum consectetur libero id faucibus nisl. Gravida neque convallis a cras semper auctor neque. In eu mi bibendum neque egestas congue quisque egestas. Pharetra diam sit amet nisl suscipit adipiscing bibendum est. Metus dictum at tempor commodo ullamcorper a. Eget nulla facilisi etiam dignissim diam quis. Blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque. A iaculis at erat pellentesque adipiscing. Pellentesque massa placerat duis ultricies lacus sed. Phasellus egestas tellus rutrum tellus.',
+    status: 'undone',
+    dueDate: new Date(),
+    team: [
+      {
+        id: '9',
+        name: 'Jukii Doe',
+        image: 'https://source.unsplash.com/607x600/?people',
+      },
+    ],
+  },
 ]
