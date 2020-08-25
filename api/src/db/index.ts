ddf0d5a763192c397b2ae7154234201810e9f181
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-25 14:36:41 +0700

    Move mock data

---
+++ api/src/db/index.ts
@@ -0,0 +1,5 @@
+import { projects } from './projectdata'
+import { tasks } from './taskdata'
+import { users } from './userdata'
+
+export { projects, tasks, users }
---
+++ api/src/db/projectdata.ts
@@ -0,0 +1,5 @@
+export const projects = [
+  { id: '1', name: 'Project 1', team: [{ id: '1', name: 'User 1' }] },
+  { id: '2', name: 'Project 2' },
+  { id: '3', name: 'Project 3', team: [{ id: '1', name: 'User 1' }] },
+]
