82d8b3c506808334c57ec95eea0406e61be369a5
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-25 16:18:44 +0700

    Implement new schema

--- api/src/schema/project/index.ts
+++ api/src/schema/project/index.ts
@@ -1,14 +1,27 @@
 import { schema } from 'nexus'
+import { Team } from '../team'
 
 export interface Project {
   id: string
-  name: string
+  projectName: string
+  projectType: string
+  projectDetail: string
+  status: string
+  dueDate: Date
+  team: Team
 }
 
 schema.objectType({
   name: 'Project',
   definition(t) {
     t.string('id')
-    t.string('name')
+    t.string('projectName')
+    t.string('projectType')
+    t.string('projectDetail')
+    t.string('status')
+    t.date('dueDate')
+    t.list.field('team', {
+      type: 'Team',
+    })
   },
 })
