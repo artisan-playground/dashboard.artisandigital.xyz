a2b71770a08d9b8239d4eb37fb1bdc6fdf689747
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-25 16:18:44 +0700

    Implement new schema

--- api/src/schema/task/index.ts
+++ api/src/schema/task/index.ts
@@ -1,14 +1,37 @@
 import { schema } from 'nexus'
+import { Comment } from '../comment'
+import { File } from '../file'
+import { Team } from '../team'
 
 export interface Task {
   id: string
+  projectId: string
   taskName: string
+  time: Date
+  taskDetail: string
+  isDone: boolean
+  team: Team
+  files: File
+  comments: Comment
 }
 
 schema.objectType({
   name: 'Task',
   definition(t) {
-    t.string('id')
+    t.string('id', { nullable: false })
+    t.string('projectId', { nullable: false })
     t.string('taskName')
+    t.date('time')
+    t.string('taskDetail')
+    t.boolean('isDone')
+    t.list.field('team', {
+      type: 'Team',
+    })
+    t.list.field('files', {
+      type: 'File',
+    })
+    t.list.field('comments', {
+      type: 'Comment',
+    })
   },
 })
--- api/src/schema/user/index.ts
+++ api/src/schema/user/index.ts
@@ -1,8 +1,10 @@
 import { schema } from 'nexus'
+import { Project } from '../project'
 
 export interface User {
   id: string
   name: string
+  projects: Project
 }
 
 schema.objectType({
@@ -10,5 +12,12 @@ schema.objectType({
   definition(t) {
     t.string('id')
     t.string('name')
+    t.list.field('projects', {
+      type: 'Project',
+      nullable: true,
+      resolve(_root, args, ctx) {
+        return ctx.db.projects.team.filter((item) => item.includes(args.id))
+      },
+    })
   },
 })
