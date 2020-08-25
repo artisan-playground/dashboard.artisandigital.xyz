afea6ff6f182844fcade25a5cba1fd816174f04c
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-25 16:17:44 +0700

    Add team schema

---
+++ api/src/schema/team/query.ts
@@ -0,0 +1,27 @@
+import { schema } from 'nexus'
+
+schema.extendType({
+  type: 'Query',
+  definition: (t) => {
+    t.field('getTeam', {
+      type: 'Team',
+      args: { id: schema.stringArg({ required: true }) },
+      resolve: (_, args, ctx) => {
+        const team = ctx.db.projects.map((p) => p.team?.filter((i) => i.id === args.id)) || null
+        return team
+      },
+    })
+  },
+})
+
+schema.extendType({
+  type: 'Query',
+  definition(t) {
+    t.list.field('getTeams', {
+      type: 'Team',
+      resolve(_, _args, ctx) {
+        return ctx.db.projects
+      },
+    })
+  },
+})
