fc9b8e6a7d89f4f7fa73ccf7c03ae5ea04897993
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-27 17:43:19 +0700

    Create Project

--- api/src/schema/project/mutation.ts
+++ api/src/schema/project/mutation.ts
@@ -1,11 +1,14 @@
-import { schema } from 'nexus'
 import { nanoid } from 'nanoid'
+import { schema } from 'nexus'
 import { Project } from '.'
 
 const InputType = schema.inputObjectType({
   name: 'CreateProjectInput',
   definition(t) {
-    t.string('name', { required: true })
+    t.string('projectName', { required: true })
+    t.string('projectType', { required: true })
+    t.string('projectDetail', { required: true })
+    t.string('projectImage', { required: true })
   },
 })
 
@@ -18,8 +21,15 @@ schema.extendType({
       resolve: (_, args, ctx) => {
         const project: Project = {
           id: nanoid(),
-          name: args.input?.name || '',
+          projectName: args.input?.projectName || '',
+          projectType: args.input?.projectType || '',
+          projectDetail: args.input?.projectDetail || '',
+          projectImage: args.input?.projectImage,
+          status: 'undone',
+          dueDate: new Date(),
+          team: [],
         }
+        ctx.db.projects.push(project)
         return project
       },
     })
