b6cc204977818457d2bb0af065a4279eb552f601
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-28 10:41:56 +0700

    Edit Project

--- api/src/schema/project/mutation.ts
+++ api/src/schema/project/mutation.ts
@@ -9,6 +9,20 @@ const InputType = schema.inputObjectType({
     t.string('projectType', { required: true })
     t.string('projectDetail', { required: true })
     t.string('projectImage', { required: true })
+    t.date('dueDate', { required: true })
+  },
+})
+
+const InputEditType = schema.inputObjectType({
+  name: 'EditProjectInput',
+  definition(t) {
+    t.string('id', { required: true })
+    t.string('projectName', { required: true })
+    t.string('projectType', { required: true })
+    t.string('projectDetail', { required: true })
+    t.string('projectImage', { required: true })
+    t.string('status', { required: true })
+    t.date('dueDate', { required: true })
   },
 })
 
@@ -26,11 +40,40 @@ schema.extendType({
           projectDetail: args.input?.projectDetail || '',
           projectImage: args.input?.projectImage,
           status: 'undone',
-          dueDate: new Date(),
+          dueDate: args.input?.dueDate || new Date(),
           team: [],
         }
         ctx.db.projects.push(project)
         return project
+      },
+    })
+  },
+})
+
+schema.extendType({
+  type: 'Mutation',
+  definition: (t) => {
+    t.field('editProject', {
+      type: 'Project',
+      args: { input: InputEditType },
+      resolve: (_, args, ctx): any => {
+        ctx.db.projects.map((item, i) => {
+          if (item.id === args.input?.id) {
+            ctx.db.projects[i] = {
+              id: args.input?.id || '',
+              projectName: args.input?.projectName || '',
+              projectType: args.input?.projectType || '',
+              projectDetail: args.input?.projectDetail || '',
+              projectImage: args.input?.projectImage,
+              status: args.input?.status || 'undone',
+              dueDate: new Date(),
+              team: [],
+            }
+            return ctx.db.projects
+          } else {
+            return new Error(`No data at id ${args.input?.id}`)
+          }
+        })
       },
     })
   },
