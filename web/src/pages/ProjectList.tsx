d1c0e94f72d19b5f2390ef8fc5bfea2eca6381b9
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-31 11:55:14 +0700

    Request data via API

--- web/src/pages/ProjectList.tsx
+++ web/src/pages/ProjectList.tsx
@@ -1,45 +1,49 @@
+import { useQuery } from '@apollo/client'
 import { Col, Input, Radio, Row, Typography } from 'antd'
 import React, { useEffect, useState } from 'react'
-import { LayoutDashboard, ProjectCard } from '../components/DashboardComponent'
-import { DATA } from '../DATA'
+import { PROJECT } from '../api'
+import { LayoutDashboard, LoadingComponent, ProjectCard } from '../components/DashboardComponent'
 
 function ProjectList() {
   const { Search } = Input
   const { Text } = Typography
+  const { loading, error, data } = useQuery(PROJECT)
 
   const [filteredData, setFilteredData] = useState<any[]>([])
   const [types, setTypes] = useState('all')
   const [keyword, setKeyword] = useState('')
-  const [loading, setLoading] = useState(false)
+  const [filterloading, setLoading] = useState(false)
 
   useEffect(() => {
-    switch (types) {
-      case 'undone':
-        let wip: any[] = DATA.filter((item) => item.status === 'undone')
-        setFilteredData(wip)
-        break
-      case 'done':
-        let closed: any[] = DATA.filter((item) => item.status === 'done')
-        setFilteredData(closed)
-        break
-      case 'all':
-        setFilteredData(DATA)
-        break
-      default:
-        setFilteredData(DATA)
-        break
+    if (!error && !loading) {
+      switch (types) {
+        case 'undone':
+          let wip: any[] = data.getProjects.filter((item: any) => item.status === 'undone')
+          setFilteredData(wip)
+          break
+        case 'done':
+          let closed: any[] = data.getProjects.filter((item: any) => item.status === 'done')
+          setFilteredData(closed)
+          break
+        case 'all':
+          setFilteredData(data.getProjects)
+          break
+        default:
+          setFilteredData(data.getProjects)
+          break
+      }
     }
-  }, [types]) // eslint-disable-line react-hooks/exhaustive-deps
+  }, [types, loading]) // eslint-disable-line react-hooks/exhaustive-deps
 
   function handleKeywordChange(e: any) {
     setLoading(true)
     setKeyword(e.target.value)
     if (e.target.value === '') {
-      setFilteredData(DATA)
+      setFilteredData(data.getProjects)
       setTypes('all')
       setLoading(false)
     } else {
-      const kw: any[] = DATA.filter((item) => {
+      const kw: any[] = data.getProjects.filter((item: any) => {
         if (types === 'all') {
           return item.projectName.toLowerCase().includes(e.target.value.toLowerCase())
         } else {
@@ -56,25 +60,6 @@ function ProjectList() {
     setLoading(false)
   }
 
-  useEffect(() => {
-    switch (types) {
-      case 'undone':
-        let wip: any[] = DATA.filter((item) => item.status === 'undone')
-        setFilteredData(wip)
-        break
-      case 'done':
-        let closed: any[] = DATA.filter((item) => item.status === 'done')
-        setFilteredData(closed)
-        break
-      case 'all':
-        setFilteredData(DATA)
-        break
-      default:
-        setFilteredData(DATA)
-        break
-    }
-  }, [types]) // eslint-disable-line react-hooks/exhaustive-deps
-
   return (
     <LayoutDashboard>
       <Row className="justify-between">
@@ -84,7 +69,7 @@ function ProjectList() {
             placeholder="input search loading default"
             value={keyword}
             onChange={handleKeywordChange}
-            loading={loading}
+            loading={filterloading}
           />
           <Radio.Group className="my-4" value={types} onChange={(e) => setTypes(e.target.value)}>
             <Radio.Button value="all">All</Radio.Button>
@@ -93,21 +78,32 @@ function ProjectList() {
           </Radio.Group>
         </Row>
       </Row>
-      <div className="site-card-wrapper">
-        <Row gutter={[8, 24]}>
-          {filteredData.length !== 0 ? (
-            filteredData.map((items, index) => (
-              <Col span={24} key={index}>
-                <ProjectCard data={items} />
-              </Col>
-            ))
-          ) : (
-            <div className="flex w-full justify-center my-8">
-              <Text disabled>Not found</Text>
-            </div>
-          )}
-        </Row>
-      </div>
+      {loading ? (
+        <LoadingComponent project />
+      ) : (
+        <div className="site-card-wrapper">
+          <Row gutter={[8, 24]}>
+            {filteredData && !error && !loading ? (
+              filteredData.length !== 0 ? (
+                filteredData.map((items, index) => (
+                  <Col span={24} key={index}>
+                    <ProjectCard data={items} />
+                  </Col>
+                ))
+              ) : (
+                <div className="flex w-full justify-center my-8">
+                  <Text disabled>No Projects Found</Text>
+                </div>
+              )
+            ) : (
+              <div className="flex w-full justify-center my-8">
+                <Text disabled>Error</Text>
+                <Text disabled>{error} </Text>
+              </div>
+            )}
+          </Row>
+        </div>
+      )}
     </LayoutDashboard>
   )
 }
