5388aea688974b2029d68b5b4e0bc1be0a6018e7
supreme5678 <tonnow_s@cmu.ac.th>
2020-08-31 11:52:25 +0700

    Create LoadingComponent

--- web/src/components/DashboardComponent.tsx
+++ web/src/components/DashboardComponent.tsx
@@ -1,19 +1,20 @@
 import EventCard from '../components/EventCard'
 import LayoutDashboard from '../components/LayoutDashboard'
-import LogCard from '../components/LogCard'
 import LayoutProfile from '../components/LayoutProfile'
+import LoadingComponent from '../components/LoadingComponent'
+import LogCard from '../components/LogCard'
 import NavBar from '../components/NavBar'
-import ProjectCard from '../components/ProjectCard'
 import ProfileForm from '../components/ProfileForm'
 import ProfileSide from '../components/ProfileSide'
 import ProfileSkillTags from '../components/ProfileSkillTags'
+import ProjectCard from '../components/ProjectCard'
 import SideNav from '../components/SideNav'
 import SideProfile from '../components/SideProfile'
-import ProfileProjectCard from './ProfileProjectCard'
-import ProfileTaskCard from './ProfileTaskCard'
 import TaskCard from '../components/TaskCard'
 import WelcomeCard from '../components/WelcomeCard'
-  
+import ProfileProjectCard from './ProfileProjectCard'
+import ProfileTaskCard from './ProfileTaskCard'
+
 export {
   EventCard,
   NavBar,
@@ -30,4 +31,5 @@ export {
   ProfileSide,
   LogCard,
   TaskCard,
+  LoadingComponent,
 }
