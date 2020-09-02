dbcf9d432ba243f3a4d89626271bc4a0cedcea9b
supreme5678 <tonnow_s@cmu.ac.th>
2020-09-02 16:10:21 +0700

    [Front-end]Implement Add, Delete comment

--- web/src/api/index.tsx
+++ web/src/api/index.tsx
@@ -190,3 +190,31 @@ export const GET_USERS = gql`
     }
   }
 `
+
+export const COMMENT = gql`
+  mutation CreateCommentInput($input: CreateCommentInput) {
+    createComment(input: $input) {
+      id
+      timestamp
+      userId
+      image
+      userImg
+      userName
+      message
+    }
+  }
+`
+
+export const DELETE_COMMENT = gql`
+  mutation Comment($commentId: String!, $taskId: String!) {
+    deleteComment(commentId: $commentId, taskId: $taskId) {
+      id
+      timestamp
+      userId
+      image
+      userImg
+      userName
+      message
+    }
+  }
+`
