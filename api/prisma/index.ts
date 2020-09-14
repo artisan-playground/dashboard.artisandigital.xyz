a51412fc3457494a582af5f9405c78a515266ff0
Kullanan <akoranos@gmail.com>
2020-09-14 15:01:15 +0700

    Update relation

--- api/prisma/index.ts
+++ api/prisma/index.ts
@@ -1,15 +1,30 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const db = new PrismaClient()
+
+main()
 
 async function main() {
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+  const users = [
+    {
+      email:"test0@mail.com",
+      name:"John Doe",
+      position:"Developer",
+    },
+    {
+      email:"test1@mail.com",
+      name:"Jane Dee",
+      position:"Developer",
+    },
+  ]
+
+  let results = []
+
+  for (const user of users) {
+    results.push(await db.user.create({ data: user }))
+  }
+
+  console.log('User: %j', results)
+
+  db.disconnect()
 }
-main()
-  .catch((e) => {
-    throw e
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
