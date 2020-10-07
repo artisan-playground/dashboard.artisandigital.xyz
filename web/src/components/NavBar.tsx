bf0130d1d9e64dcc9953a84a9adc8adf91ea7435
Kullanan <akoranos@gmail.com>
2020-10-07 11:07:27 +0700

    Update navbar

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,38 +1,49 @@
 import { BellOutlined, DownOutlined } from '@ant-design/icons'
-import { Avatar, Badge, Dropdown, Layout, Menu, Row, Typography } from 'antd'
-import React from 'react'
+import { Avatar, Badge, Button, Dropdown, Layout, Menu, Row, Typography } from 'antd'
+import React, { useEffect, useState } from 'react'
 import { Link as RouterLink } from 'react-router-dom'
-import { DATA, TASK_DATA } from '../DATA'
 import { useStoreActions, useStoreState } from '../store'
 import '../styles/main.css'
+import { useQuery } from '@apollo/client'
+import { TASKS } from '../services/api/task'
 
 function NavBar() {
   const { Header } = Layout
   const { Text, Link } = Typography
   const user = useStoreState((s) => s.userState.user)
   const logout = useStoreActions((a) => a.userState.logOut)
+  const { loading, error, data } = useQuery(TASKS)
+  const [filteredData, setFilteredData] = useState<any[]>([])
+
+  useEffect(() => {
+    if (!error && !loading) {
+      setFilteredData(data.tasks)
+    }
+  }, [data, error, loading])
 
   async function onLogoutClick() {
     await logout()
   }
   const notifications = (
-    <Menu>
-      {TASK_DATA.filter((task: any) => task.team.filter((team: any) => team.id === '1')).map(
-        (mapItem) => (
+    <Menu className="p-2">
+      {filteredData
+        .filter((task: any) => task.members.filter((member: any) => member.id === user?.id))
+        .map((mapItem: any) => (
           <Menu.Item key={mapItem.projectId}>
             <RouterLink
               to={{
                 pathname: `/projects/${mapItem.projectId}`,
                 state: {
-                  data: DATA[mapItem.projectId],
+                  data: data[mapItem.projectId],
                 },
               }}
             >
               <Text>{mapItem.taskName}</Text>
             </RouterLink>
           </Menu.Item>
-        )
-      )}
+        ))}
+      <Menu.Divider />
+      <Link>Mark As Read</Link>
     </Menu>
   )
 
@@ -48,6 +59,7 @@ function NavBar() {
           Edit Profile
         </RouterLink>
       </Menu.Item>
+      <Menu.Divider />
       <Menu.Item>
         <Link onClick={onLogoutClick}>Log out</Link>
       </Menu.Item>
@@ -73,16 +85,11 @@ function NavBar() {
         </div>
 
         <Menu theme="light" mode="horizontal" selectable={false} className="w-auto h-16">
-          <Menu.Item key="2" className="font-bold">
+          <Menu.Item key="1" className="font-bold">
             <Dropdown overlay={userDropDown} placement="bottomCenter" arrow>
               <Row className="justify-center items-center">
                 <div className="block hover:hidden">
-                  <Avatar
-                    src={user?.image}
-                    className="border-2 mr-2 "
-                    alt="user"
-                    size="large"
-                  />
+                  <Avatar src={user?.image} className="border-2 mr-2 " alt="user" size="large" />
                   {user?.name}
                 </div>
                 <div className="hover:block hidden">
@@ -92,7 +99,7 @@ function NavBar() {
             </Dropdown>
           </Menu.Item>
 
-          <Menu.Item key="1">
+          <Menu.Item key="2">
             <Dropdown
               className="px-2 py-0 w-full"
               overlay={notifications}
@@ -103,8 +110,8 @@ function NavBar() {
                 <BellOutlined style={{ fontSize: 24 }} />
                 <Badge
                   count={
-                    TASK_DATA.filter((task: any) =>
-                      task.team.filter((taskTeam: any) => taskTeam.id === '1')
+                    filteredData.filter((task: any) =>
+                      task.members.filter((member: any) => member.id === user?.id)
                     ).length
                   }
                   className="-ml-4 -mt-4"
