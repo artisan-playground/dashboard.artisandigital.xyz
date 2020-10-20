0eb05e63a773465d8138bcb4bdd43818fd887a5e
Kullanan <akoranos@gmail.com>
2020-10-20 11:33:00 +0700

    Add mutation to change status

--- web/src/pages/ProjectDetail.tsx
+++ web/src/pages/ProjectDetail.tsx
@@ -4,6 +4,8 @@ import {
   ProfileOutlined,
   ScheduleOutlined,
   SmileOutlined,
+  CheckCircleOutlined,
+  ExclamationCircleOutlined,
 } from '@ant-design/icons'
 import { useMutation, useQuery } from '@apollo/client'
 import { Button, Card, Col, Input, Row, Typography } from 'antd'
@@ -23,6 +25,7 @@ import {
   UPDATE_PROJECT_NAME,
   UPDATE_PROJECT_DETAIL,
   UPDATE_PROJECT_TYPE,
+  UPDATE_PROJECT_STATUS,
 } from '../services/api/project'
 
 function ProjectDetail() {
@@ -40,16 +43,19 @@ function ProjectDetail() {
     loading: projectLoading,
     error: projectError,
     data: projectData,
+    refetch: projectRefetch,
   } = useQuery(GET_PROJECT_BY_ID, { variables: { id: Number(projectId) } })
   const [updateProjectName] = useMutation(UPDATE_PROJECT_NAME)
   const [updateProjectDetail] = useMutation(UPDATE_PROJECT_DETAIL)
   const [updateProjectType] = useMutation(UPDATE_PROJECT_TYPE)
+  const [updateProjectStatus] = useMutation(UPDATE_PROJECT_STATUS)
   const ref = useRef(document.createElement('div'))
   const { TextArea } = Input
 
   const [projectName, setProjectName] = useState<any>()
   const [projectDetail, setProjectDetail] = useState<any>()
   const [projectType, setProjectType] = useState<any>()
+  const [status, setStatus] = useState<any>()
   const [editProjectName, setEditProjectName] = useState(false)
   const [editProjectDetail, setEditProjectDetail] = useState(false)
   const [editProjectType, setEditProjectType] = useState(false)
@@ -66,6 +72,7 @@ function ProjectDetail() {
       setProjectName(projectData.project.projectName)
       setProjectDetail(projectData.project.projectDetail)
       setProjectType(projectData.project.projectType)
+      setStatus(projectData.project.status)
     }
   }, [projectId, loading, error, projectLoading, projectError, data, projectData])
 
@@ -81,7 +88,6 @@ function ProjectDetail() {
       setEditProjectName(false)
       setEditProjectDetail(false)
       setEditProjectType(false)
-      refetch()
     }
   }
 
@@ -89,46 +95,89 @@ function ProjectDetail() {
     setDrawerVisible(false)
   }
 
-  return (
+  function onUnDoneClick() {
+    updateProjectStatus({
+      variables: { id: Number(projectId), status: 'undone' },
+    })
+    projectRefetch()
+  }
+
+  function onDoneClick() {
+    updateProjectStatus({
+      variables: { id: Number(projectId), status: 'done' },
+    })
+    projectRefetch()
+  }
+
+  return projectLoading || !filteredData ? (
+    <LoadingComponent overlay />
+  ) : (
     <LayoutDashboard noCard>
       <Row className="w-full">
         <Row className="w-full">
           <Col span={24}>
             <Row className="w-full">
               <Col span={24} lg={{ span: 4 }} className="flex justify-center items-start">
-                <Avatar size={112} src={filteredData ? filteredData.projectImage : ''} />
+                <Avatar size={112} src={filteredData.projectImage} />
               </Col>
               <Col span={24} lg={{ span: 20 }} className="px-4">
-                <Row>
-                  {!editProjectName ? (
-                    <div
-                      onClick={() => {
-                        setEditProjectName(true)
-                      }}
+                <Row justify="space-between">
+                  <div>
+                    {!editProjectName ? (
+                      <div
+                        onClick={() => {
+                          setEditProjectName(true)
+                        }}
+                      >
+                        <Text className="font-bold text-3xl ml-2">{projectName}</Text>
+                      </div>
+                    ) : (
+                      <Input
+                        className="font-bold text-3xl ml-2"
+                        autoFocus
+                        defaultValue={projectName}
+                        onChange={(e) => {
+                          if (filteredData) {
+                            setProjectName(e.target.value)
+                            updateProjectName({
+                              variables: { id: Number(projectId), projectName: e.target.value },
+                            })
+                          }
+                        }}
+                      />
+                    )}
+                  </div>
+                  {filteredData.status === 'done' ? (
+                    <Button
+                      className="flex items-center justify-center shadow-md hover:shadow-lg bg-green-400 focus:bg-green-600 hover:bg-red-600 transition duration-800 ease-in border-0 w-24"
+                      type="primary"
+                      shape="round"
+                      size="large"
+                      onClick={onUnDoneClick}
                     >
-                      <Text className="font-bold text-3xl ml-2">{projectName}</Text>
-                    </div>
+                      <CheckCircleOutlined className="hover:hidden" />
+                      <Text className="hover:block hidden text-white">Done</Text>
+                    </Button>
                   ) : (
-                    <Input
-                      className="font-bold text-3xl ml-2"
-                      autoFocus
-                      defaultValue={projectName}
-                      onChange={(e) => {
-                        if (filteredData) {
-                          setProjectName(e.target.value)
-                          updateProjectName({
-                            variables: { id: Number(projectId), projectName: e.target.value },
-                          })
-                        }
-                      }}
-                    />
+                    <Button
+                      className="flex items-center justify-center shadow-md hover:shadow-lg bg-red-400 focus:bg-red-600 hover:bg-green-600 transition duration-800 ease-in border-0 w-24"
+                      type="primary"
+                      shape="round"
+                      size="large"
+                      onClick={onDoneClick}
+                    >
+                      <ExclamationCircleOutlined />
+                      <Text className="hover:hidden text-white">WIP</Text>
+                    </Button>
                   )}
                 </Row>
                 <Row>
                   {!editProjectType ? (
-                    <div onClick={() => {
-                      setEditProjectType(true)
-                    }}>
+                    <div
+                      onClick={() => {
+                        setEditProjectType(true)
+                      }}
+                    >
                       <Text className="text-md mt-4 mb-2 text-gray-500">{projectType}</Text>
                     </div>
                   ) : (
@@ -186,14 +235,11 @@ function ProjectDetail() {
                       style={{ color: '#105EFC', fontSize: '2.5rem', marginBottom: 8 }}
                     />
                     <Title level={3} className="text-center">
-                      {new Date(filteredData ? filteredData.dueDate : '').toLocaleDateString(
-                        'en-GB',
-                        {
-                          day: 'numeric',
-                          month: 'short',
-                          year: 'numeric',
-                        }
-                      )}
+                      {new Date(filteredData.dueDate).toLocaleDateString('en-GB', {
+                        day: 'numeric',
+                        month: 'short',
+                        year: 'numeric',
+                      })}
                     </Title>
                     <Text disabled className="text-md -mt-2 text-center">
                       Release Date
@@ -208,7 +254,7 @@ function ProjectDetail() {
                       style={{ color: '#105EFC', fontSize: '2.5rem', marginBottom: 8 }}
                     />
                     <Title level={3} className="text-center">
-                      {filteredData ? filteredData.members.length : ''}
+                      {filteredData.members.length}
                     </Title>
                     <Text disabled className="text-md -mt-2 text-center">
                       Developer
@@ -267,7 +313,7 @@ function ProjectDetail() {
                   <TaskDrawer
                     visibillity={drawerVisible}
                     onCloseDrawer={closeDawer}
-                    project={filteredData ? filteredData : ''}
+                    project={filteredData}
                     refetch={() => refetch()}
                   />
                 </div>
@@ -279,7 +325,7 @@ function ProjectDetail() {
                     <TaskCard
                       key={item.id}
                       data={item}
-                      project={filteredData ? filteredData : ''}
+                      project={filteredData}
                       refetch={() => refetch()}
                     />
                   ))
