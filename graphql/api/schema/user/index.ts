0232382d444d7de13866fbaf9f2b86d210db4c41
Kullanan <akoranos@gmail.com>
2020-10-27 11:54:01 +0700

    Update setting vscode

--- graphql/api/schema/user/index.ts
+++ graphql/api/schema/user/index.ts
@@ -1,20 +1,20 @@
 import { schema } from 'nexus'
 
 schema.objectType({
-    name: 'User',
-    definition(t) {
-      t.model.id()
-      t.model.email()
-      t.model.name()
-      t.model.image()
-      t.model.position()
-      t.model.department()
-      t.model.type()
-      t.model.skills()
-      t.model.contacts()
-      t.model.projects()
-      t.model.tasks()
-      t.model.startDate()
-      t.model.dueDate()
-    },
-  })
+  name: 'User',
+  definition(t) {
+    t.model.id()
+    t.model.email()
+    t.model.name()
+    t.model.image()
+    t.model.position()
+    t.model.department()
+    t.model.type()
+    t.model.skills()
+    t.model.contacts()
+    t.model.projects()
+    t.model.tasks()
+    t.model.startDate()
+    t.model.dueDate()
+  },
+})
