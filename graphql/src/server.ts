03a25a84939ee58a7d7363d00842216700c66988
Kullanan <akoranos@gmail.com>
2020-10-30 11:34:45 +0700

    Update apollo server

---
+++ graphql/src/server.ts
@@ -0,0 +1,8 @@
+import { ApolloServer } from "apollo-server";
+import { createContext } from "./context";
+import { schema } from "./schema";
+const port = process.env.PORT || 4000;
+new ApolloServer({ schema, context: createContext }).listen(
+  { port: port },
+  () => console.log(`🚀 Server ready at: http://localhost:${port}`)
+);
