ceaee9657456c4b48430ffda87ebc55b195da45f
Kullanan <akoranos@gmail.com>
2020-10-30 11:13:32 +0700

    Update prisma schema

--- graphql/api/schema/file/query..ts
+++
@@ -1,9 +0,0 @@
-import { schema } from 'nexus'
-
-schema.extendType({
-  type: 'Query',
-  definition(t) {
-    t.crud.file()
-    t.crud.files()
-  },
-})
--- graphql/api/schema/user/query..ts
+++
@@ -1,9 +0,0 @@
-import { schema } from 'nexus'
-
-schema.extendType({
-  type: 'Query',
-  definition(t) {
-    t.crud.user()
-    t.crud.users()
-  },
-})
