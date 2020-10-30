7f23caa6babfdd35bfaeb4d2d4e79298396a65a4
Kullanan <akoranos@gmail.com>
2020-10-30 14:28:05 +0700

    Add require and configure dotenv

--- graphql/src/server.ts
+++ graphql/src/server.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from "apollo-server";
 import { createContext } from "./context";
 import { schema } from "./schema";
+
 const port = process.env.PORT || 4000;
 new ApolloServer({ schema, context: createContext }).listen(
   { port: port },
