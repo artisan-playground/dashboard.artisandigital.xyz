074c5e5cd461000fc3beeffe8dbcae54ec2714ed
Kullanan Thanachotnan <akoranos@gmail.com>
2020-11-17 18:02:46 +0700

    Add file to service api

---
+++ web/src/services/api/file.ts
@@ -0,0 +1,65 @@
+import { gql } from '@apollo/client'
+
+export const GET_FILES = gql`
+  query {
+    files {
+      id
+      fileName
+      path
+      fullPath
+      endpoint
+      extension
+      task {
+        id
+      }
+    }
+  }
+`
+
+export const GET_FILE = gql`
+  query getImage($id: Int!) {
+    file(where: { id: $id }) {
+      id
+      fileName
+      path
+      fullPath
+      endpoint
+      extension
+      task {
+        id
+      }
+    }
+  }
+`
+
+export const UPLOAD_FILE = gql`
+  mutation UploadFile($id: Int!, $file: Upload!) {
+    uploadFile(taskId: { connect: { id: $id } }, file: $file) {
+      id
+      fileName
+      path
+      fullPath
+      endpoint
+      extension
+      task {
+        id
+      }
+    }
+  }
+`
+
+export const UPDATE_FILE = gql`
+  mutation UpdateFile($id: Int!, $file: Upload!) {
+    updateFile(id: $id, file: $file) {
+      id
+      fileName
+      path
+      fullPath
+      endpoint
+      extension
+      task {
+        id
+      }
+    }
+  }
+`
