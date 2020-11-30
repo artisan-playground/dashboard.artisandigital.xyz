538b0468c5f2f7021b01eddd53d2c243f666ad2a
chalobon <chalobon-jaje@hotmail.com>
2020-11-30 18:15:34 +0700

    Add login page and edit mention in create project

--- line-liff/app-vue/src/views/login.vue
+++ line-liff/app-vue/src/views/login.vue
@@ -1,135 +1,70 @@
 <template>
-  <div class="login">
-    <img alt="Vue logo" src="../assets/logo.svg" width="110" height="110" class="logo" />
-    <!-- <h1>Artisan</h1>
-    <h1 style="margin-top:0px">Digital</h1>-->
-    <div class="content">
-      <!-- <md-field>
-        <md-icon>
-          <span class="iconify" data-inline="false" data-icon="clarity:user-line"></span>
-        </md-icon>
-        <label>Email</label>
-        <md-input v-model="email"></md-input>
-      </md-field>
-
-      <md-field id="password">
-        <md-icon>
-          <span class="iconify" data-inline="false" data-icon="clarity:lock-line"></span> </md-icon
-        >&nbsp; &nbsp;
-        <label>Password</label>
-        <md-input type="password" v-model="password"></md-input>
-      </md-field>
-      <a href="/forgetpassword" id="forgetpass" style="float:right">Forget password?</a>
-
-      <md-button id="button-login" class="md-raised md-primary">login</md-button>
-      <a href="/register">
-        <span style="color:black; ">Don't have account?</span>
-        Register?
-      </a> -->
+  <div class="element">
+    <div class="subelement">
+      <img class="logo" src="../assets/logo-login.svg" />
+      <a-form
+        id="components-form-demo-normal-login"
+        :form="form"
+        class="login-form"
+        @submit="handleSubmit"
+      >
+        <a-form-item>
+          <a-input
+            v-decorator="[
+              'userName',
+              { rules: [{ required: true, message: 'Please input your username!' }] },
+            ]"
+            placeholder="Username"
+            addon-after="@artisan.co.th"
+          >
+            <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-button
+            style="color:white; background-color:#0036C7;"
+            html-type="submit"
+            class="login-form-button"
+          >
+            Log in
+          </a-button>
+        </a-form-item>
+      </a-form>
+      <div class="sub-element">
+        <img src="../../src/assets/background-login.svg" alt="" />
+      </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'login',
-  props: {
-    msg: String,
-  },
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<script></script>
 <style scoped>
-h1 {
-  font-family: Avenir;
-  text-transform: uppercase;
-  font-style: normal;
-  font-weight: 700;
-  font-size: 24px;
-  line-height: 100.6%;
-  /* or 25px */
-
-  text-align: center;
-  letter-spacing: 0.07em;
-  margin: 10px auto 3px auto;
-  /* top right bottom left */
-  /* margin: 10px 10px 15px 20px; */
-}
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-.content a {
-  /* color: #42b983; */
-  color: #0036c7;
-  font-size: 12px;
-  font-weight: 950;
-}
-
-.logo {
-  margin-bottom: 40px; /* 50px คือระยะห่าง logo กับ email */
-}
-
-.content {
-  /* top right bottom left */
-  /* margin: 10px 10px 15px 20px; */
-  margin-left: 40px;
-  margin-right: 40px;
-  margin-top: 2px;
-  align-content: center;
-}
-
-#password {
-  width: 100%;
-  min-height: 48px;
-  margin: 4px 0 15px;
-  padding-top: 16px;
+.element {
+  /* display: -webkit-flex;
   display: flex;
-  position: relative;
-  font-family: inherit;
-}
-
-#button-login {
-  width: 100%;
-  background-color: #105efb;
-  height: 42px;
-  border-radius: 8px;
-  text-transform: capitalize;
-  margin-bottom: 15px; /* ระยะห่างระหว่าง login กับ Don't have account? Register? */
-}
-
-/* เส้นใต้ icons input */
-.md-field > .md-icon:after {
-  width: 37px;
-  height: 4px;
-  position: absolute;
-  left: -1px;
-  bottom: -5px;
-  transition: 0.3s cubic-bezier(0.4, 0, 0.2, 1);
-  content: none;
-}
-
-.md-field {
-  width: 100%;
-  min-height: 48px;
-  margin: 4px 0 15px;
-  /* ระยะห่างของ input แต่ละอัน */
-  padding-top: 16px;
+  flex-direction: column; */
+  min-height: 100vh;
   display: flex;
-  position: relative;
-  font-family: inherit;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
 }
-
-#forgetpass {
-  margin-bottom: 40px;
-  /* ระยะห่างระหว่าง forgetpassword กับ ปุ่ม login */
+.subelement {
+  margin-left: 15px;
+  margin-right: 15px;
+}
+.sub-element {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+#components-form-demo-normal-login .login-form {
+  max-width: 300px;
+}
+#components-form-demo-normal-login .login-form-forgot {
+  float: right;
+}
+#components-form-demo-normal-login .login-form-button {
+  width: 100%;
 }
 </style>
