42629e562c297f438c62e96cb25adc493d1c9481
Kullanan Thanachotnan <akoranos@gmail.com>
2020-12-22 11:42:21 +0700

    Add createAt model

--- graphql/src/api/project/objectType.ts
+++ graphql/src/api/project/objectType.ts
@@ -1,18 +1,19 @@
-import { objectType } from "@nexus/schema";
+import { objectType } from '@nexus/schema'
 
 const projectObjectType = objectType({
-  name: "Project",
+  name: 'Project',
   definition(t) {
-    t.model.id();
-    t.model.projectName();
-    t.model.projectType();
-    t.model.projectDetail();
-    t.model.projectImage();
-    t.model.status();
-    t.model.dueDate();
-    t.model.members();
-    t.model.tasks();
+    t.model.id()
+    t.model.projectName()
+    t.model.projectType()
+    t.model.projectDetail()
+    t.model.projectImage()
+    t.model.status()
+    t.model.dueDate()
+    t.model.createAt()
+    t.model.members()
+    t.model.tasks()
   },
-});
+})
 
-export { projectObjectType };
+export { projectObjectType }
