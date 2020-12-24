e06f3993808cba16356c7f6ecc2175be99ca717f
Kullanan Thanachotnan <akoranos@gmail.com>
2020-12-24 20:31:06 +0700

    Update config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,20 +1,32 @@
 module.exports = {
-  purge: false,
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: {
+    content: ['./src/**/*.tsx'],
+  },
   theme: {
     backgroundColor: (theme) => ({
       ...theme('colors'),
-      primary: '#105EFC',
+      primary: '#0036C7',
+      secondary: '#134F83',
+      third: 'rgb(233, 240, 255)',
       primaryopacity: 'rgba(16,94,252,0.5)',
-      secondary: '#fff',
       danger: '#e3342f',
       success: '#1BED09',
       successop: 'rgba(27,237,9,0.5)',
       progress: '#FFF500',
       progressop: 'rgba(255,245,0,0.5)',
-      file: '#f9f9f9',
     }),
+    extend: {
+      spacing: {
+        72: '18rem',
+        84: '21rem',
+        96: '24rem',
+      },
+    },
   },
-
   variants: {},
   plugins: [],
 }
