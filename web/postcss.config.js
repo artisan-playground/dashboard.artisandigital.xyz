2f9cb3b3d197d133bf2587d7a6632b1a714d07c9
Kullanan Thanachotnan <akoranos@gmail.com>
2020-12-24 20:31:06 +0700

    Update config

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   plugins: [
     require('tailwindcss')('./tailwind.config.js'),
-    process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
-    process.env.NODE_ENV === 'production'
-      ? require('@fullhuman/postcss-purgecss')({
-          content: ['./src/*.tsx', './src/**/*.tsx'],
-          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-        })
-      : null,
     process.env.NODE_ENV === 'production' ? require('cssnano')({ preset: 'default' }) : null,
   ],
 }
