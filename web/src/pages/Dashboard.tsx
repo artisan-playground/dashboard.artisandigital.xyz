38466d73dbbbe87cd0a05e0b051838d25790b6fa
Kullanan Thanachotnan <akoranos@gmail.com>
2020-12-24 20:55:23 +0700

    Re design dashboard page

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,17 +1,22 @@
-import { FundProjectionScreenOutlined, ProfileOutlined, TeamOutlined } from '@ant-design/icons'
+import {
+  FileProtectOutlined,
+  FundProjectionScreenOutlined,
+  UsergroupAddOutlined,
+} from '@ant-design/icons'
 import { useQuery } from '@apollo/client'
-import { Avatar, Card, Col, List, Row, Typography } from 'antd'
+import { Calendar, Card, Col, Empty, Row, Typography } from 'antd'
 import { Axis, Chart, Coordinate, Interaction, Interval, Tooltip } from 'bizcharts'
+import dayjs from 'dayjs'
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { LayoutDashboard, LoadingComponent, WelcomeCard } from '../components/DashboardComponent'
+import { LayoutDashboard, TodayTaskCard, WelcomeCard } from '../components/DashboardComponent'
 import { PROJECT } from '../services/api/project'
 import { TASKS } from '../services/api/task'
 import { GET_USERS, GET_USER_BY_ID } from '../services/api/user'
 import { useStoreState } from '../store'
 
 function Dashboard() {
-  const { Title, Text } = Typography
+  const { Text } = Typography
   const { loading, data: projectData } = useQuery(PROJECT)
   const { data: taskData } = useQuery(TASKS)
   const { data: userData } = useQuery(GET_USERS)
@@ -23,12 +28,7 @@ function Dashboard() {
 
   const overDue = currentUserData
     ? currentUserData.projects.filter(
-        (item: any) =>
-          item.status === 'undone' &&
-          item.dueDate <
-            new Date().toLocaleDateString('en-US', {
-              day: 'numeric',
-            })
+        (item: any) => item.status === 'undone' && item.dueDate < dayjs().format('DD MMM YYYY')
       ).length
     : 0
 
@@ -93,165 +93,192 @@ function Dashboard() {
 
   return (
     <LayoutDashboard noCard>
-      <div>
-        <div className="site-card-wrapper">
-          <Row gutter={[10, 24]}>
-            <Col md={{ span: 24 }}>
-              {currentUserData ? (
-                <WelcomeCard
-                  data={currentUserData}
-                  project={calProjects()}
-                  task={
-                    taskData
-                      ? taskData.tasks.filter(
-                          (task: any) =>
-                            task.isDone === false &&
-                            task.members.filter((member: any) => member.id === user?.id).length
-                        ).length
-                      : 0
-                  }
-                />
-              ) : (
-                0
-              )}
-            </Col>
-          </Row>
-          <Row gutter={[14, 24]}>
-            <Col xl={14} className="w-full">
-              <Card className="mb-4">
-                <Link to="/projects">
-                  <Card.Grid>
-                    <div className="flex flex-col justify-center items-center">
-                      <FundProjectionScreenOutlined
-                        style={{ color: '#105EFC', fontSize: '40px', marginBottom: 8 }}
-                      />
-                      <Title level={2}>
-                        {projectData
-                          ? projectData.projects.filter((project: any) =>
-                              project.members.filter((member: any) => member.id === user?.id)
-                            ).length
-                          : 0}
-                      </Title>
-                      <Text disabled className="text-lg -mt-4">
-                        Projects
-                      </Text>
-                    </div>
-                  </Card.Grid>
-                </Link>
-                <Card.Grid>
-                  <Link to="/member">
-                    <div className="flex flex-col justify-center items-center">
-                      <TeamOutlined
-                        style={{ color: '#105EFC', fontSize: '40px', marginBottom: 8 }}
-                      />
-                      <Title level={2}>{userData ? userData.users.length : 0}</Title>
-                      <Text disabled className="text-lg -mt-4">
-                        Paticipants
-                      </Text>
-                    </div>
-                  </Link>
-                </Card.Grid>
-                <Card.Grid>
-                  <Link to={`/profile/${user?.id}`}>
-                    <div className="flex flex-col justify-center items-center">
-                      <ProfileOutlined
-                        style={{ color: '#105EFC', fontSize: '40px', marginBottom: 8 }}
-                      />
-                      <Title level={2}>
-                        {taskData
+      <div className="px-8 py-8">
+        <Row justify="space-between">
+          <Col xs={15}>
+            <div className="site-card-wrapper">
+              <Row className="mb-8">
+                <Col xs={24}>
+                  {currentUserData ? (
+                    <WelcomeCard
+                      data={currentUserData}
+                      project={calProjects()}
+                      task={
+                        taskData
                           ? taskData.tasks.filter(
                               (task: any) =>
                                 task.isDone === false &&
                                 task.members.filter((member: any) => member.id === user?.id).length
                             ).length
-                          : 0}
-                      </Title>
-                      <Text disabled className="text-lg -mt-4">
-                        Today's tasks
-                      </Text>
-                    </div>
-                  </Link>
-                </Card.Grid>
-              </Card>
-
-              {loading ? (
-                <LoadingComponent projects />
-              ) : (
-                <List
-                  className="bg-white p-4"
-                  header={<div>Active Projects</div>}
-                  bordered={false}
-                  dataSource={
-                    projectData &&
-                    projectData.projects.filter(
-                      (filtered: any) =>
-                        filtered.members.filter((member: any) => member.id === user?.id).length
-                    )
-                  }
-                  renderItem={(items: any) => (
-                    <List.Item>
-                      <Avatar
-                        src={
-                          items.projectImage
-                            ? items.projectImage.fullPath
-                            : require('../assets/images/logo5.png')
-                        }
-                        className="mr-4"
+                          : 0
+                      }
+                    />
+                  ) : null}
+                </Col>
+              </Row>
+              <Row>
+                <Col className="w-full">
+                  <Card title="Project Charts">
+                    {projectData ? (
+                      <Chart
+                        height={250}
+                        data={charts}
+                        scale={cols}
+                        autoFit
+                        onGetG2Instance={(c: any) => {
+                          c.geometries[0].elements.forEach((e: any, idx: any) => {
+                            e.setState('selected', idx === 0 ? true : false)
+                          })
+                        }}
+                      >
+                        <Coordinate type="theta" radius={0.75} />
+                        <Tooltip showTitle={false} />
+                        <Axis visible={false} />
+                        <Interval
+                          position="percent"
+                          adjust="stack"
+                          color="item"
+                          style={{
+                            lineWidth: 1,
+                            stroke: '#fff',
+                          }}
+                          label={[
+                            'count',
+                            {
+                              content: (data: any) => {
+                                if (overDue === 0) {
+                                  const val = ((data.percent / value) * 100).toFixed(2) + '%'
+                                  return `${data.item}: ${val}`
+                                } else {
+                                  const val = ((data.percent / valueOverall) * 100).toFixed(2) + '%'
+                                  return `${data.item}: ${val}`
+                                }
+                              },
+                            },
+                          ]}
+                        />
+                        <Interaction type="element-single-selected" />
+                      </Chart>
+                    ) : (
+                      <Empty
+                        image={Empty.PRESENTED_IMAGE_SIMPLE}
+                        description={<Text disabled>No Chart Now</Text>}
                       />
-                      {items.projectName}
-                    </List.Item>
+                    )}
+                  </Card>
+                </Col>
+
+                <Col className="w-full mt-8">
+                  <Text className="font-bold text-lg">Today’s task</Text>
+                  {taskData && !error && !loading ? (
+                    taskData.length !== 0 ? (
+                      taskData.tasks
+                        .filter(
+                          (task: any) =>
+                            task.members.filter((member: any) => member.id === user?.id).length
+                        )
+                        .map((items: any) => (
+                          <Col span={24} key={items.id}>
+                            <TodayTaskCard data={items} />
+                          </Col>
+                        ))
+                    ) : (
+                      <div className="flex w-full justify-center my-8">
+                        <Empty
+                          image={Empty.PRESENTED_IMAGE_SIMPLE}
+                          description={<Text disabled>Don't have any task to do</Text>}
+                        />
+                      </div>
+                    )
+                  ) : (
+                    <div className="flex w-full justify-center my-8">
+                      <Text disabled>Error</Text>
+                      <Text disabled>{error} </Text>
+                    </div>
                   )}
-                />
-              )}
-            </Col>
+                </Col>
+              </Row>
+            </div>
+          </Col>
 
-            <Col xl={10} xs={24}>
-              <Card title="Project Charts">
-                <Chart
-                  height={250}
-                  data={charts}
-                  scale={cols}
-                  autoFit
-                  onGetG2Instance={(c: any) => {
-                    c.geometries[0].elements.forEach((e: any, idx: any) => {
-                      e.setState('selected', idx === 0 ? true : false)
-                    })
-                  }}
-                >
-                  <Coordinate type="theta" radius={0.75} />
-                  <Tooltip showTitle={false} />
-                  <Axis visible={false} />
-                  <Interval
-                    position="percent"
-                    adjust="stack"
-                    color="item"
-                    style={{
-                      lineWidth: 1,
-                      stroke: '#fff',
-                    }}
-                    label={[
-                      'count',
-                      {
-                        content: (data) => {
-                          if (overDue === 0) {
-                            const val = ((data.percent / value) * 100).toFixed(2) + '%'
-                            return `${data.item}: ${val}`
-                          } else {
-                            const val = ((data.percent / valueOverall) * 100).toFixed(2) + '%'
-                            return `${data.item}: ${val}`
-                          }
-                        },
-                      },
-                    ]}
-                  />
-                  <Interaction type="element-single-selected" />
-                </Chart>
-              </Card>
-            </Col>
-          </Row>
+          <Col xs={8}>
+            <div className="site-calendar-customize-header-wrapper border">
+              <Calendar fullscreen={false} />
+            </div>
 
-          <Row gutter={[8, 24]}></Row>
-        </div>
+            <Col className="w-full">
+              <Link to="/projects">
+                <Card hoverable className="my-4">
+                  <Row>
+                    <Col className="flex items-center justify-center">
+                      <FundProjectionScreenOutlined
+                        style={{ color: '#0036C7', fontSize: '40px', marginBottom: 8 }}
+                      />
+                    </Col>
+                    <Col className="ml-4">
+                      <Text type="secondary" className="text-lg block">
+                        Projects
+                      </Text>
+                      <Text className="text-lg font-bold block">
+                        {projectData
+                          ? projectData.projects.filter((project: any) =>
+                              project.members.filter((member: any) => member.id === user?.id)
+                            ).length
+                          : 0}
+                      </Text>
+                    </Col>
+                  </Row>
+                </Card>
+              </Link>
+
+              <Link to={`/profile/${user?.id}`}>
+                <Card hoverable className="my-4">
+                  <Row>
+                    <Col className="flex items-center justify-center">
+                      <FileProtectOutlined
+                        style={{ color: '#0036C7', fontSize: '40px', marginBottom: 8 }}
+                      />
+                    </Col>
+                    <Col className="ml-4">
+                      <Text className="text-lg -mt-4 block" type="secondary">
+                        Complete tasks
+                      </Text>
+                      <Text className="text-lg font-bold block">
+                        {taskData
+                          ? taskData.tasks.filter(
+                              (task: any) =>
+                                task.isDone === true &&
+                                task.members.filter((member: any) => member.id === user?.id).length
+                            ).length
+                          : 0}
+                      </Text>
+                    </Col>
+                  </Row>
+                </Card>
+              </Link>
+
+              <Link to="/employee">
+                <Card hoverable className="my-4">
+                  <Row>
+                    <Col className="flex items-center justify-center">
+                      <UsergroupAddOutlined
+                        style={{ color: '#0036C7', fontSize: '40px', marginBottom: 8 }}
+                      />
+                    </Col>
+                    <Col className="ml-4">
+                      <Text type="secondary" className="text-lg -mt-4 block">
+                        Paticipants
+                      </Text>
+                      <Text className="text-lg font-bold block">
+                        {userData ? userData.users.length : 0}
+                      </Text>
+                    </Col>
+                  </Row>
+                </Card>
+              </Link>
+            </Col>
+          </Col>
+        </Row>
       </div>
     </LayoutDashboard>
   )
