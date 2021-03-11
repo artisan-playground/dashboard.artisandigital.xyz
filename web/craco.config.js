95fdf6b743fec8d70232441f6203d7cc5c272cb2
Kullanan Thanachotnan <akoranos@gmail.com>
2021-03-11 14:49:17 +0700

    Update theme

--- web/craco.config.js
+++ web/craco.config.js
@@ -10,6 +10,9 @@ module.exports = {
             modifyVars: {
               '@primary-color': '#134F83',
               '@menu-item-active-bg': '#e9f0ff',
+              '@select-item-selected-bg': '#e9f0ff',
+              '@picker-basic-cell-active-with-range-color': '#e9f0ff',
+              '@calendar-item-active-bg': '#e9f0ff',
             },
             javascriptEnabled: true,
           },
