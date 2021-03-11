e161d25910eccb397706b0860c51221f7bd19c3c
Kullanan Thanachotnan <akoranos@gmail.com>
2021-03-11 17:14:53 +0700

    Fix server

--- graphql/src/server.ts
+++ graphql/src/server.ts
@@ -1,18 +1,8 @@
-import { ApolloServer } from 'apollo-server-express'
-import express from 'express'
+import { ApolloServer } from 'apollo-server'
 import { createContext } from './context'
 import { schema } from './schema'
 
 const port = process.env.PORT || 4000
-const server = new ApolloServer({ schema, context: createContext })
-
-const app = express()
-
-var corsOptions = {
-  origin: process.env.DOMAIN,
-  credentials: true,
-}
-
-server.applyMiddleware({ app, cors: corsOptions })
-
-app.listen({ port: port }, () => console.log(`🚀 Server ready at http://localhost:${port}`))
+new ApolloServer({ schema, context: createContext }).listen({ port: port }, () =>
+  console.log(`🚀 Server ready at: http://localhost:${port}`)
+)
